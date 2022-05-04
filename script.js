33053e0f268a4f4573de0b2ed5281bfd9cae5283
Glitch (pingobras) <none>
2022-05-04 12:46:16 +0000

    🐛🎮 Checkpoint ./javascript:26026384/232 ./script.js:26026384/232 ./download.html:26026384/1038

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ if (btn){
     btn.classList.toggle("dipped");
   };}
 
+<a href="https://www.webcontadores.com" title="contador para site"><img src="https://counter9.stat.ovh/private/webcontadores.php?c=zcrmrdqsuts62apq8xn82nu44gqpxtpj" border="0" title="contador para site" alt="contador para site"></a>
