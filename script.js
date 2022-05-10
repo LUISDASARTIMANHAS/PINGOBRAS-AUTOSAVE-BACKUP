31b9651b5c74dfa630fc25b51b52ff90d4f129e8
Glitch (pingobras) <none>
2022-05-10 15:14:34 +0000

    🐝🐲 Checkpoint ./style.css:26026384/26 ./download.html:26026384/335 ./script.js:26026384/401

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ function barButton() {
     }
   }
 }
+
+var i = 0;
+function barButton() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("factoriobar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
