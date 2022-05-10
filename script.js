73c8c1d42a70089fb0004c82c9a972691f26a37c
Glitch (pingobras) <none>
2022-05-10 14:36:38 +0000

    🍤🍞 Checkpoint ./script.js:26026384/1163 ./download.html:26026384/22

--- script.js
+++ script.js
@@ -7,34 +7,21 @@ if (btn){
     btn.classList.toggle("dipped");
   };}
 
-
-var bar = new ProgressBar.Line(#file, {
-  strokeWidth: 4,
-  easing: 'easeInOut',
-  duration: 1400,
-  color: '#FFEA82',
-  trailColor: '#eee',
-  trailWidth: 1,
-  svgStyle: {width: '100%', height: '100%'},
-  text: {
-    style: {
-      // Text color.
-      // Default: same as stroke color (options.color)
-      color: '#999',
-      position: 'absolute',
-      right: '0',
-      top: '30px',
-      padding: 0,
-      margin: 0,
-      transform: null
-    },
-    autoStyleContainer: false
-  },
-  from: {color: '#FFEA82'},
-  to: {color: '#ED6A5A'},
-  step: (state, bar) => {
-    bar.setText(Math.round(bar.value() * 100) + ' %');
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("#file");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
   }
-});
-
-bar.animate(1.0);  // Number from 0.0 to 1.0
+}
