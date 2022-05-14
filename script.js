d8412550b9737f06c3891009e73a07caf1d7ca79
Glitch (pingobras) <none>
2022-05-14 12:23:20 +0000

    🛀🐑 Checkpoint ./script.js:26026384/101

--- script.js
+++ script.js
@@ -50,12 +50,13 @@ function factorioButton() {
 
 let client = JSON.parse(localStorage.getItem('client') || '[]')
 client.push({
-      luis: '[name: "luis das artimanhas",saldo: 3.50]',
+      client: {
+      luis: {name: "luis das artimanhas",saldo: 3.50},
       lukinhas: {name: "lukinhas",saldo:3.50},
       douglas: {name: "douglasPG",saldo: 13.00},
       valdo: {name: "valdo9645",saldo: -3.00},
       vm: {name: "skip",saldo: 1.00},
-      eduardo: {name: "dudu",saldo: 2.00},
+      eduardo: {name: "dudu",saldo: 2.00},}
 })
 const DJson = JSON.stringify(client);
 localStorage.setItem("JSON STORAGE", DJson);
@@ -63,5 +64,4 @@ localStorage.setItem("JSON STORAGE", DJson);
 
 let STORAGE = localStorage.getItem("JSON STORAGE");
 let CR = JSON.parse(STORAGE);
-document.getElementById("console").innerHTML = CR.luis;
         
