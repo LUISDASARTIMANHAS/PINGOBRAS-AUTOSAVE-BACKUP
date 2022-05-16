47cfa54c22fd93b5a005184c4313cee80547f70b
Glitch (pingobras) <none>
2022-05-16 12:11:08 +0000

    👯💺 Checkpoint ./script.js:26026384/2 ./index.html:26026384/221 ./style.css:26026384/46

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ window.addEventListener("load", function(){
     setTimeout(
         function open(event){
             document.querySelector(".popup").style.display = "block";
-        },3000
+        },2000
     )
 });
 document.querySelector("#close").addEventListener("click", function(){
