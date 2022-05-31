25806b9bd069da83ea5047566567fcc3b6442d0f
Glitch (pingobras) <none>
2022-05-31 16:58:22 +0000

    🍦🐧 Checkpoint ./script.js:26026384/12 ./index.html:26026384/167 ./text/javascript.javascript:26026384/160

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ Descer.addEventListener("click", function() {
 window.addEventListener("load", function(){
 setTimeout(
 function open(event){
-document.querySelector(".popup").style.display = "visible";},5000)
+document.querySelector(".popup").style.display = "block";},5000)
 });
 document.querySelector("#close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
