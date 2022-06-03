2299b48d14f3bda6710ce4197137d27770f2df69
Glitch (pingobras) <none>
2022-06-03 12:23:05 +0000

    🙇🚟 Checkpoint ./script.js:26026384/91

--- script.js
+++ script.js
@@ -58,14 +58,14 @@ document.querySelector("#close").addEventListener("click", function(){
 let client = JSON.parse(localStorage.getItem('client') || '[]')
 
 client.push(
-  {client: {
+  {
       luis: {name: "luis das artimanhas",saldo: 3.50},
       lukinhas: {name: "lukinhas",saldo:3.50},
       douglas: {name: "douglasPG",saldo: 13.00},
       valdo: {name: "valdo9645",saldo: -3.00},
       vm: {name: "skip",saldo: 1.00},
       eduardo: {name: "dudu",saldo: 2.00},
-           }})
+           })
 const DJson = JSON.stringify(client);
 localStorage.setItem("JSON STORAGE", DJson);
 
@@ -73,5 +73,6 @@ localStorage.setItem("JSON STORAGE", DJson);
 let STORAGE = localStorage.getItem("JSON STORAGE");
 let CR = JSON.parse(STORAGE);
 console.log(CR);
+let UserVm = JSON.parse(CR.vm.name);
 
 
