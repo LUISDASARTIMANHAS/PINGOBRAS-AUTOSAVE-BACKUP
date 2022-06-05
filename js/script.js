19730456ce95c38938d09b783b7af7499dcef8f7
Glitch (pingobras) <none>
2022-06-05 15:26:02 +0000

    🕌🏵 Checkpoint ./js/script.js:26026384/75

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ batepapoDescer.addEventListener("click", function() {
 });
 
 function autentication() {
-window.location.href = ("autentication=user=true.html");
+window.location.href = ("https://pingobras-autentication.glitch.me");
 };
 
 
