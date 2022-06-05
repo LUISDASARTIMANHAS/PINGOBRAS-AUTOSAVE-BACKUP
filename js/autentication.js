f4a49a803f83275bdf96f1395f5ec9fe0639f772
Glitch (pingobras) <none>
2022-06-05 02:55:04 +0000

    💂🚵 Checkpoint ./js/autentication.js:26026384/4416 ./autentication=user=true.html:26026384/103

--- js/autentication.js
+++ js/autentication.js
@@ -1,20 +1,27 @@
-let senha = document.querySelector("#AdminPassword")
-let msgError = document.querySelector("#msgError")
-
-function enviar(){ 
-console.log("func-enviar");
-if(senha == 123456){
+function enviar(){
+    let senha = document.querySelector('#senha')
+  
+  let msgError = document.querySelector('#msgError')
+  let listaUser = []
+  
+  let key = 12345
+  if(senha.value == key.senha){
     document.documentElement.requestFullscreen();
     window.location.href = 'https://codepen.io/luisdasartimanhas/full/ZEaVByy'
     
     let mathRandom = Math.random().toString(16).substr(2)
     let token = mathRandom + mathRandom
     
-    localStorage.setItem('token', token)}
+    localStorage.setItem('token', token)
+    localStorage.setItem('Jsonkeys', JSON.stringify(key))}
+  
   else {
     senha.setAttribute('style', 'color: red')
     senha.setAttribute('style', 'border-color: red')
     msgError.setAttribute('style', 'display: block')
     msgError.innerHTML = 'Usuário ou senha incorretos'
+   
   }
 }
+
+function refresh()
