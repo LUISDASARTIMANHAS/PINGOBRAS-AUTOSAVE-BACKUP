37061cdbce502be9051d251652538538e07f86ae
Glitch (pingobras) <none>
2022-06-05 16:04:51 +0000

    ⛹🍙 Checkpoint ./js/script.js:26026384/663 ./js/STORAGE.js:26026384/658 ./index.html:26026384/62 ./download.html:26026384/46

---
+++ js/STORAGE.js
@@ -0,0 +1,13 @@
+let client = JSON.parse(localStorage.getItem('client') || '[]')
+
+client.push(
+  {
+      luis: {name: "luis das artimanhas",saldo: 3.50},
+      lukinhas: {name: "lukinhas",saldo:3.50},
+      douglas: {name: "douglasPG",saldo: 13.00},
+      valdo: {name: "valdo9645",saldo: -3.00},
+      vm: {name: "skip",saldo: 1.00},
+      eduardo: {name: "dudu",saldo: 2.00},
+           })
+const DJson = JSON.stringify(client);
+localStorage.setItem("JSON STORAGE", DJson);
--- js/script.js
+++ js/script.js
@@ -62,25 +62,6 @@ document.querySelector("#close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 });
 
-
-
-let client = JSON.parse(localStorage.getItem('client') || '[]')
-
-client.push(
-  {
-      luis: {name: "luis das artimanhas",saldo: 3.50},
-      lukinhas: {name: "lukinhas",saldo:3.50},
-      douglas: {name: "douglasPG",saldo: 13.00},
-      valdo: {name: "valdo9645",saldo: -3.00},
-      vm: {name: "skip",saldo: 1.00},
-      eduardo: {name: "dudu",saldo: 2.00},
-           })
-const DJson = JSON.stringify(client);
-localStorage.setItem("JSON STORAGE", DJson);
-
-
 let STORAGE = localStorage.getItem("JSON STORAGE");
 let CR = JSON.parse(STORAGE);
 console.log(CR);
-
-
