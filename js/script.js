6581ca98825b78f5a3ea2a3049a886e7b96afaa0
Glitch (pingobras) <none>
2022-06-09 16:27:43 +0000

    🚘🐃 Checkpoint ./index.html:26026384/636 ./js/script.js:26026384/623

--- js/script.js
+++ js/script.js
@@ -91,3 +91,25 @@ if (btndisabled){
 let YTRe = JSON.parse(YoutubeLinks);
 document.getElementById("console").innerHTML = YTRe.LINK1;
 console.log(YTRe);
+
+
+document.addEventListener("DOMContentLoaded", function(){
+				var audio = document.getElementById("audio");
+				
+				document.getElementById("play").addEventListener("click", function(){
+					audio.play();
+					console.log("Play");
+				}, false);
+				
+				document.getElementById("pause").addEventListener("click", function(){
+					audio.pause();
+					console.log("pause");
+				}, false);
+				
+				document.getElementById("volume").addEventListener("mousemove", function(){
+					var volume = document.getElementById("volume").value;
+					audio.volume = volume;
+					console.log(volume);
+				}, false);
+				
+			}, false);
