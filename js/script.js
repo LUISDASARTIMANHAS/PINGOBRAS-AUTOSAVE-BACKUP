0cef30feaaa78f2644282d3ed2650a79d917c6b8
Glitch (pingobras) <none>
2022-06-09 16:51:01 +0000

    ⛹🍢 Checkpoint ./index.html:26026384/206 ./js/script.js:26026384/290

--- js/script.js
+++ js/script.js
@@ -96,20 +96,8 @@ console.log(YTRe);
 document.addEventListener("DOMContentLoaded", function(){
 				var audio = document.getElementById("audio");
 				
-				document.getElementById("play").addEventListener("click", function(){
-					audio.play();
-					console.log("Play");
-				}, false);
-				
-				document.getElementById("pause").addEventListener("click", function(){
-					audio.pause();
-					console.log("pause");
-				}, false);
-				
 				document.getElementById("volume").addEventListener("mousemove", function(){
 					var volume = document.getElementById("volume").value;
 					audio.volume = volume;
 					console.log(volume);
-				}, false);
-				
-			}, false);
+				}, false);}, false);
