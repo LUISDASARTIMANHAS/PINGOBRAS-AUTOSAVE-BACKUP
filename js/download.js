71a870abde12b162fd2a863a94af5ae83884fdb0
Glitch (pingobras) <none>
2022-06-10 10:44:19 +0000

    🚥🎯 Checkpoint ./download.html:26026384/1426 ./js/download.js:26026384/1279

--- js/download.js
+++ js/download.js
@@ -38,6 +38,66 @@ function factorioButton() {
   }
 }
 
+
+	(function($) {
+
+
+		var config = {
+    "component": {
+        "path": "dumping_grounds\/upload",
+        "data": {
+            "dump_id": null
+        }
+    },
+    "sizeLimit": 256000000,
+    "dropAreaText": "Drop file, or click here to upload.",
+    "multiple": false,
+    "allowedExtensions": [
+        "bmp",
+        "gif",
+        "jpg",
+        "jpeg",
+        "png",
+        "tif",
+        "tiff",
+        "webp",
+        "swf",
+        "fla",
+        "swc",
+        "as",
+        "doc",
+        "htm",
+        "html",
+        "hx",
+        "odt",
+        "rtf",
+        "txt",
+        "xml",
+        "ace",
+        "gz",
+        "rar",
+        "tar",
+        "zip",
+        "pdf",
+        "psd"
+    ],
+    "liveUpdate": true,
+    "template": "upload_template_627a97320a83d"
+};
+		var uploader = new ngutils.uploader("#uploader627a97320a83b", "\/parkfile", config);
+		
+			})(jQuery);
+ngutils.event.addListener(ngutils.uploadEvent.onComplete, function(event) {
+			event.target.remove();
+			if (event.data.response.error) {
+				alert(event.data.response.error);
+			}
+		});
+		ngutils.event.addListener(ngutils.uploadEvent.onError, function(event) {
+			event.target.remove();
+			alert(event.data.errorMsg);
+		});
+
 function contato() {
-  document.getElementById("frm1").submit();
+  document.getElementById("contato").submit();
 }
