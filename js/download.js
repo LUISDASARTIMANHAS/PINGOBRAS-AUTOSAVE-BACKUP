3db9b64e55ed9660ac09ca2c0bd61350bfe19a44
Glitch (pingobras) <none>
2022-06-29 14:40:18 +0000

    💘💧 Checkpoint ./js/download.js:26026384/75 ./download.html:26026384/15

--- js/download.js
+++ js/download.js
@@ -3,7 +3,7 @@ function factorioButton() {
   if (i == 0) {
     i = 1;
     var elem = document.getElementById("factoriobar");
-    var width = 10;
+    var width = 331000;
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
@@ -28,6 +28,7 @@ function barButton() {
     var id = setInterval(frame, 100);
     function frame() {
       if (width >= 100) {
+        window.location.href = "https://pingobras.glitch.me"
         clearInterval(id);
         i = 0;
       } else {
