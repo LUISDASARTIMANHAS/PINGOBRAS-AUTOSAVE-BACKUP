6e24bebf8b003d10ae010e9821a374df7b3479bb
Glitch (pingobras) <none>
2022-07-05 12:00:05 +0000

    🛤🕶 Checkpoint ./js/webshare.js:26026384/741 ./webshare.html:26026384/1143

--- js/webshare.js
+++ js/webshare.js
@@ -1,3 +1,19 @@
 const btn = document.querySelector('#share');
 const resultPara = document.querySelector('#result');
 
+        navigator.mediaDevices.then(stream => {
+            screenTrack = new Twilio.Video.LocalVideoTrack(stream.getTracks()[0]);
+            room.localParticipant.publishTrack(screenTrack);
+        }).catch(() => {
+            alert('Não foi possivel compartilhar a tela.')
+        });
+room.localParticipant.unpublishTrack(screenTrack);
+        screenTrack.stop();
+        screenTrack = null;
+
+function participantConnected(participant) {
+    var labelDiv = document.createElement('div');
+    labelDiv.setAttribute('class', 'label');
+    labelDiv.innerHTML = participant.identity;
+    participantDiv.appendChild(labelDiv);
+  
