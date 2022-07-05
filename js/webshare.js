152e6e5abbcc9b8c0cc9dbcdbcd7f76f6bd5c416
Glitch (pingobras) <none>
2022-07-05 12:11:04 +0000

    🐪🏁 Checkpoint ./js/webshare.js:26026384/1481

--- js/webshare.js
+++ js/webshare.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector('#share');
 const resultPara = document.querySelector('#result');
+const shareScreen = document.getElementById('share_screen');
+var screenTrack;
 
         navigator.mediaDevices.then(stream => {
             screenTrack = new Twilio.Video.LocalVideoTrack(stream.getTracks()[0]);
@@ -16,4 +18,42 @@ function participantConnected(participant) {
     labelDiv.setAttribute('class', 'label');
     labelDiv.innerHTML = participant.identity;
     participantDiv.appendChild(labelDiv);
-  
+  }
+
+shareScreen.addEventListener('click', shareScreenHandler);
+function shareScreenHandler() {
+    event.preventDefault();
+    if (!screenTrack) {
+        navigator.mediaDevices.getDisplayMedia().then(stream => {
+            screenTrack = new Twilio.Video.LocalVideoTrack(stream.getTracks()[0]);
+            room.localParticipant.publishTrack(screenTrack);
+            shareScreen.innerHTML = 'Stop sharing';
+            screenTrack.mediaStreamTrack.onended = () => { shareScreenHandler() };
+        }).catch(() => {
+            alert('Could not share the screen.');
+        });
+    }
+    else {
+        room.localParticipant.unpublishTrack(screenTrack);
+        screenTrack.stop();
+        screenTrack = null;
+        shareScreen.innerHTML = 'Share screen';
+    }
+};
+function connectButtonHandler(event) {
+    event.preventDefault();
+    if (!connected) {
+        // ...
+        connect(username).then(() => {
+            // ...
+            shareScreen.disabled = false;
+        }).catch(() => {
+        // ...
+       });
+    }
+   else {
+        disconnect();
+        shareScreen.innerHTML = 'Share screen';
+        shareScreen.disabled = true;
+    }
+};
