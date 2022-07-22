009738dbb99fbbf5fed280755675c1059ae7d8d2
Glitch (pingobras) <none>
2022-07-22 13:39:48 +0000

    📈🌗 Checkpoint ./js/time.js:26026384/560 ./index.html:26026384/60

--- js/time.js
+++ js/time.js
@@ -4,7 +4,7 @@ const looptime = setInterval(timeRefresh, 500);
 let secondsplayer = "15"
 let setminutesplayer = "30"
 let minutesplayer = setminutesplayer < 10 ? "0" + setminutesplayer : setminutesplayer;
-let sethoursplayer = "19"
+let sethoursplayer = "00"
 let hoursplayer = sethoursplayer < 10 ? "0" + sethoursplayer : sethoursplayer;
 
 const horaselect = hoursplayer + minutesplayer 
@@ -18,20 +18,25 @@ let Getminutes = time.getMinutes();
 let minutes = Getminutes < 10 ? "0" + Getminutes : Getminutes;
 let Getseconds = time.getSeconds();
 let seconds = Getseconds < 10 ? "0" + Getseconds : Getseconds;
-  
 let horario = hours +":" + minutes +":" + seconds
 let horariojs = hours +""+ minutes 
 
+//< para zerar o relogio caso fique negativo ou ultrapasseo horario definido
 let setfaultS = -seconds + 60;
 let faultS = setfaultS < 0 ? "00" : setfaultS;
 let setfaultM = -minutes + 60;
 let faultM = setfaultM < 0 ? "00" : setfaultM;
 let setfaultH = 19 - hours;   
 let faultH = setfaultH < 0 ?  "00" : setfaultH;
-let dispalyH = setfaultH < 0 ?  "00" : setfaultH;
+//>  
   
+//< para ficar igual relogio digital  
+let dispalyS = faultS < 10 ?  "0" + faultS : faultS;  
+let dispalyM = faultM < 10 ?  "0" + faultM : faultM;  
+let dispalyH = faultH < 10 ?  "0" + faultH : faultH;
+//>
   
-const fault = faultH+":" + faultM+":" + faultS
+const fault = dispalyH+":" + dispalyM+":" + dispalyS
 document.querySelector("[fault]").innerHTML = fault;
 
   
@@ -42,9 +47,11 @@ document.querySelector("[timer]").innerHTML = horario;
   if(horariojs > horaselect){
     document.querySelector(".site-altura").style.display = "none";
     document.querySelector(".msg-error-hour").style.display = "block";
+    document.querySelector("#auto").innerHTML = "<p>" + "text auto" + "</p>"
   }else{
     document.querySelector(".site-altura").style.display = "block";
     document.querySelector(".msg-error-hour").style.display = "none";
+    
   }
 
   
