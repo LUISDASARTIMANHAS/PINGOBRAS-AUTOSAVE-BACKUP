01ad66a0a700b7f335768a2b2c957edd1109afbc
Glitch (pingobras) <none>
2022-07-22 13:27:48 +0000

    🎇🍢 Checkpoint ./js/time.js:26026384/220

--- js/time.js
+++ js/time.js
@@ -7,7 +7,7 @@ let minutesplayer = setminutesplayer < 10 ? "0" + setminutesplayer : setminutesp
 let sethoursplayer = "19"
 let hoursplayer = sethoursplayer < 10 ? "0" + sethoursplayer : sethoursplayer;
 
-const horaselect = hoursplayer + minutesplayer + secondsplayer
+const horaselect = hoursplayer + minutesplayer 
       
 function timeRefresh() {
 const time = new Date();
@@ -20,13 +20,16 @@ let Getseconds = time.getSeconds();
 let seconds = Getseconds < 10 ? "0" + Getseconds : Getseconds;
   
 let horario = hours +":" + minutes +":" + seconds
-let horariojs = hours + minutes + seconds
+let horariojs = hours +""+ minutes 
 
-let faultS = -seconds + 60;
+let setfaultS = -seconds + 60;
+let faultS = setfaultS < 0 ? "00" : setfaultS;
 let setfaultM = -minutes + 60;
 let faultM = setfaultM < 0 ? "00" : setfaultM;
 let setfaultH = 19 - hours;   
 let faultH = setfaultH < 0 ?  "00" : setfaultH;
+let dispalyH = setfaultH < 0 ?  "00" : setfaultH;
+  
   
 const fault = faultH+":" + faultM+":" + faultS
 document.querySelector("[fault]").innerHTML = fault;
