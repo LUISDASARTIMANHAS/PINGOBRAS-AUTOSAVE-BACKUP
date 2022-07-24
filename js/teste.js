3b3464e44e0cbab0bd2b3a91e1e2be90ccb4a8a7
Glitch (pingobras) <none>
2022-07-24 16:52:53 +0000

    🏤🏙 Checkpoint ./css/style.css:26026384/402 ./films&series.html:26026384/60 ./js/teste.js:26026384/714

---
+++ js/teste.js
@@ -0,0 +1,17 @@
+let sec = "20"
+let ms = "000"
+
+const looprgb = setInterval(rgb, 20000);
+let ytVideo = document.querySelector(".youtube-video")
+
+
+function rgb() {
+  ytVideo.style.border = "5px solid red"
+//document.querySelector("[horaselect]").innerHTML = hoursplayer+":" + minutesplayer+":" + secondsplayer;  
+//document.querySelector("[horario]").innerHTML = horario;
+//document.querySelector("[timer]").innerHTML = horario;
+
+
+//
+
+}
