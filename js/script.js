fa75df9d4484795ed6ada5ff945c595d6a69e879
Glitch (pingobras) <none>
2022-07-24 17:47:52 +0000

    💂🏈 Checkpoint ./js/script.js:26026384/1674

--- js/script.js
+++ js/script.js
@@ -101,44 +101,67 @@ const looprgb8 = setInterval(purple, 8000);
 let RGB1 = document.querySelector("[RGB1]")
 let RGB2 = document.querySelector("[RGB2]")
 let RGB3 = document.querySelector("[RG3]")
+let CRGB1 = document.querySelector("[CRGB1]")
+let CRGB2 = document.querySelector("[CRGB2]")
+RGB1.style.transition = "all 0.3s ease-out"
+RGB2.style.transition = "all 0.3s ease-out"
+RGB3.style.transition = "all 0.3s ease-out"
+CRGB1.style.transition = "all 0.3s ease-out"
+CRGB2.style.transition = "all 0.3s ease-out"
 
 function red() {
   RGB1.style.borderColor = "red"
   RGB2.style.borderColor = "red"
   RGB3.style.borderColor = "red"
+  CRGB1.style.borderColor = "red"
+  CRGB2.style.borderColor = "red"
 }
 function orange() {
   RGB1.style.borderColor = "orange"
   RGB2.style.borderColor = "orange"
   RGB3.style.borderColor = "orange"
+  CRGB1.style.borderColor = "orange"
+  CRGB2.style.borderColor = "orange"
 }
 function yellow() {
   RGB1.style.borderColor = "yellow"
   RGB2.style.borderColor = "yellow"
   RGB3.style.borderColor = "yellow"
+  CRGB1.style.borderColor = "yellow"
+  CRGB2.style.borderColor = "yellow"
 }
 function green() {
   RGB1.style.borderColor = "green"
   RGB2.style.borderColor = "green"
   RGB3.style.borderColor = "green"
+  CRGB1.style.borderColor = "green"
+  CRGB2.style.borderColor = "green"
 }
 function blue() {
   RGB1.style.borderColor = "blue"
   RGB2.style.borderColor = "blue"
   RGB3.style.borderColor = "blue"
+  CRGB1.style.borderColor = "blue"
+  CRGB2.style.borderColor = "blue"
 }
 function darkblue() {
   RGB1.style.borderColor = "darkblue"
   RGB2.style.borderColor = "darkblue"
   RGB3.style.borderColor = "darkblue"
+  CRGB1.style.borderColor = "darkblue"
+  CRGB2.style.borderColor = "darkblue"
 }
 function lightblue() {
   RGB1.style.borderColor = "lightblue"
   RGB2.style.borderColor = "lightblue"
   RGB3.style.borderColor = "lightblue"
+  CRGB1.style.borderColor = "lightblue"
+  CRGB2.style.borderColor = "lightblue"
 }
 function purple() {
   RGB1.style.borderColor = "purple"
   RGB2.style.borderColor = "purple"
   RGB3.style.borderColor = "purple"
+  CRGB1.style.borderColor = "purple"
+  CRGB2.style.borderColor = "purple"
 }
