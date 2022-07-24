d1a265756571ee29b14d1e46a06952011bc0d4a4
Glitch (pingobras) <none>
2022-07-24 17:04:44 +0000

    👗🏁 Checkpoint ./js/teste.js:26026384/1497

--- js/teste.js
+++ js/teste.js
@@ -1,17 +1,20 @@
-let sec = "20"
+let sec = "10"
 let ms = "000"
 
-const looprgb = setInterval(rgb, 20000);
+const looprgb = setInterval(red, 5000);
+const looprgb2 = setInterval(blue, 10000);
+const looprgb3 = setInterval(green, 15000);
 let ytVideo = document.querySelector(".youtube-video")
-
-
-function rgb() {
-  ytVideo.style.border = "5px solid red"
 //document.querySelector("[horaselect]").innerHTML = hoursplayer+":" + minutesplayer+":" + secondsplayer;  
 //document.querySelector("[horario]").innerHTML = horario;
 //document.querySelector("[timer]").innerHTML = horario;
 
-
-//
-
+function red() {
+ytVideo.style.border = "5px solid red"
+}
+function blue() {
+ytVideo.style.border = "5px solid blue"
+}
+function green() {
+ytVideo.style.border = "5px solid green"
 }
