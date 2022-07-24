ff0c3ba52ebbefe907fc09ebbce832758caf47bb
Glitch (pingobras) <none>
2022-07-24 17:16:50 +0000

    🎖🌈 Checkpoint ./js/teste.js:26026384/1603

--- js/teste.js
+++ js/teste.js
@@ -1,20 +1,52 @@
 let sec = "10"
 let ms = "000"
 
-const looprgb = setInterval(red, 5000);
-const looprgb2 = setInterval(blue, 10000);
-const looprgb3 = setInterval(green, 15000);
-let ytVideo = document.querySelector(".youtube-video")
+const looprgb = setInterval(red, 1000);
+const looprgb2 = setInterval(orange, 2000);
+const looprgb3 = setInterval(yellow, 3000);
+const looprgb4 = setInterval(green, 4000);
+const looprgb5 = setInterval(blue, 5000);
+const looprgb6 = setInterval(darkblue, 6000);
+const looprgb7 = setInterval(lightblue, 7000);
+let RGB1 = document.querySelector("[RGB1]")
+let RGB2 = document.querySelector("[RGB2]")
+let RGB3 = document.querySelector("[RG3]")
 //document.querySelector("[horaselect]").innerHTML = hoursplayer+":" + minutesplayer+":" + secondsplayer;  
 //document.querySelector("[horario]").innerHTML = horario;
 //document.querySelector("[timer]").innerHTML = horario;
 
 function red() {
-ytVideo.style.border = "5px solid red"
+  RGB1.style.border = "5px solid red"
+  RGB2.style.border = "5px solid red"
+  RGB3.style.border = "5px solid red"
 }
-function blue() {
-ytVideo.style.border = "5px solid blue"
+function orange() {
+  RGB1.style.border = "5px solid orange"
+  RGB2.style.border = "5px solid orange"
+  RGB3.style.border = "5px solid orange"
+}
+function yellow() {
+  RGB1.style.border = "5px solid yellow"
+  RGB2.style.border = "5px solid yellow"
+  RGB3.style.border = "5px solid yellow"
 }
 function green() {
-ytVideo.style.border = "5px solid green"
+  RGB1.style.border = "5px solid green"
+  RGB2.style.border = "5px solid green"
+  RGB3.style.border = "5px solid green"
+}
+function blue() {
+  RGB1.style.border = "5px solid blue"
+  RGB2.style.border = "5px solid blue"
+  RGB3.style.border = "5px solid blue"
+}
+function darkblue() {
+  RGB1.style.border = "5px solid darkblue"
+  RGB2.style.border = "5px solid darkblue"
+  RGB3.style.border = "5px solid darkblue"
+}
+function lightblue() {
+  RGB1.style.border = "5px solid lightblue"
+  RGB2.style.border = "5px solid lightblue"
+  RGB3.style.border = "5px solid lightblue"
 }
