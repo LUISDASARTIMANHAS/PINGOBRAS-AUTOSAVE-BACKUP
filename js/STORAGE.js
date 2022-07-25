15bfd9456d9a28269aaacdae372080140c31108f
Glitch (pingobras) <none>
2022-07-25 00:06:31 +0000

    🌟🌝 Checkpoint ./js/STORAGE.js:26026384/1193

--- js/STORAGE.js
+++ js/STORAGE.js
@@ -3,15 +3,15 @@ const loopStorage = setInterval(refreshStorage, 15000);
 function refreshStorage() {
 let client = JSON.parse(localStorage.getItem('client') || '[]')
 console.warn("STORAGE/LOG> client pegou dados de client antigo!");
-client.push(
-  {
+client.push({
       luis: {name: "luis das artimanhas",saldo: 3.50},
-      lukinhas: {name: "lukinhas",saldo:3.50},
-      douglas: {name: "douglasPG",saldo: 13.00},
-      valdo: {name: "valdo9645",saldo: -3.00},
-      vm: {name: "skip",saldo: 1.00},
-      eduardo: {name: "dudu",saldo: 2.00},
-           })
+        lukinhas: {name: "lukinhas",saldo:3.50},
+        douglas: {name: "douglasPG",saldo: 13.00},
+        valdo: {name: "valdo9645",saldo: -3.00},
+        vm: {name: "skip",saldo: 1.00},
+        Eduardo: {name: "dudu",saldo: 2.00},
+        José: {name: "José Eduardo Batista de Souza",saldo: 1.50, senha: 10062002}
+  })
 console.warn("STORAGE/LOG> client adcionou novos itens");
 const DJson = JSON.stringify(client);
 localStorage.setItem("JSON STORAGE", DJson);
