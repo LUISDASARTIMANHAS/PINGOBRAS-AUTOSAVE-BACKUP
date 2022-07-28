297e69b6227a379468b19aa7a18cb2b5ea9b0a20
Glitch (pingobras) <none>
2022-07-28 11:17:49 +0000

    🐿🎽 Checkpoint ./js/calcs.js:26026384/203 ./calcs&dividendos.html:26026384/72 ./css/style.css:26026384/12

--- js/calcs.js
+++ js/calcs.js
@@ -4,5 +4,7 @@ let bonus = JSON.parse(rebonusStorage);
 let siteMoney = 36.00
 let userbonus = siteMoney - bonus.moeda
 
-document.getElementById("currentSaldo").innerHTML = userbonus
+//document.getElementById("currentSaldo").innerHTML = userbonus
    
+const inputbuscar = document.getElementById("inputbuscar")
+console.log(inputbuscar.value)
