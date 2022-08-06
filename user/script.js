82dffb5ece5b623020d67f6441b1b8aa9a1f9c9f
Glitch (pingobras) <none>
2022-08-06 18:35:46 +0000

    📹🚃 Checkpoint ./user/script.js:26026384/320 ./index.html:26026384/130 ./user/index.html:26026384/53

--- user/script.js
+++ user/script.js
@@ -17,16 +17,14 @@ btntema.addEventListener('click', function() {
   });
 
 
-
-
-let userLogado = JSON.parse(localStorage.getItem('userLogado')) 
-let logado = document.querySelector('#logado')
-logado.innerHTML = "Olá" + userLogado.nome
-
 if(localStorage.getItem('token') == null){
   alert('Você precisa estar logado para acessar essa página')
   window.location.href = '/login'
 }
+let userLogado = JSON.parse(localStorage.getItem('userLogado')) 
+let logado = document.querySelector('#logado')
+logado.innerHTML = "Olá " + userLogado.nome + " Bem Vindo de Volta!"
+
 function sair(){
   localStorage.removeItem('token')
   localStorage.removeItem('userLogado')
