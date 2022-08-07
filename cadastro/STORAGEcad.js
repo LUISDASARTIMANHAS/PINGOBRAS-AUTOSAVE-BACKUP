23ae4933c3815d2bd7bef0217ef22b07097ebf98
Glitch (pingobras) <none>
2022-08-07 15:17:24 +0000

    ⛸💳 Checkpoint ./cadastro/STORAGEcad.js:26026384/555

--- cadastro/STORAGEcad.js
+++ cadastro/STORAGEcad.js
@@ -5,10 +5,13 @@ function storagecad() {
   
   if(storage == null){
   alert("null user auto complete online")
-   let listacad = [{nomeCad: "administrador", userCad: "equipe", senhaCad: "administrador", saldoCad: 1.00,}]
+   let listacad = [{nomeCad: "administrador", userCad: "equipe", senhaCad: "administrador", saldoCad: 1.00},
+                   {nomeCad: "Florisvaldo de Oliveira Schulz", userCad: "Valdo", senhaCad: "96451290", saldoCad: 5.00}]
    let refreshcad = JSON.stringify(listacad)
    localStorage.setItem( 'listaUser',refreshcad);
    }else{
-   localStorage.setItem( 'listaUser',storage); }
+   let storageadd = storage.push[{nomeCad: "administrador", userCad: "equipe", senhaCad: "administrador", saldoCad: 1.00},
+                   {nomeCad: "Florisvaldo de Oliveira Schulz", userCad: "Valdo", senhaCad: "96451290", saldoCad: 5.00}]
+   localStorage.setItem( 'listaUser',storageadd); }
   
   }
