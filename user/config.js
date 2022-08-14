2cf05fcef5195cbdc4ae4ce7a90419fef1b5cbbc
Glitch (pingobras) <none>
2022-08-14 11:35:31 +0000

    ⏰🐜 Checkpoint ./carteira/index.html:26026384/958 ./css/style.css:26026384/314 ./user/account.html:26026384/310 ./user/config.js:26026384/31 ./user/index.html:26026384/717

--- user/config.js
+++ user/config.js
@@ -5,7 +5,7 @@ let inputColor = document.querySelector('#colorTema');
 let userConta = document.getElementById("userConta") 
 let nomeConta = document.getElementById("nomeConta")
 let senhaConta = document.getElementById("senhaConta") 
-let saldoConta = document.getElementById("saldoConta") 
+let saldoConta = document.getElementById("header_wallet_balance") 
 let IDConta = document.getElementById("IDConta")
 let Logado = JSON.parse(localStorage.getItem('userLogado'))
 
