2bb8acff7f630c4be675746e6a421f943d2e5337
Glitch (pingobras) <none>
2022-08-21 16:48:34 +0000

    🚤⏱ Checkpoint ./user/films&series.html:26026384/5 ./js/calcs.js:26026384/1710 ./js/functions.js:26026384/1456 ./calcs&dividendos.html:26026384/241

--- js/calcs.js
+++ js/calcs.js
@@ -1,11 +1,14 @@
 let rebonusStorage = localStorage.getItem("siteBonus");
 let bonus = JSON.parse(rebonusStorage);
+const ClickMouseCalc = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/click%20do%20mouse.mp3?v=1661006466474');
 
 let siteMoney = 36.00
 let userbonus = siteMoney - bonus.moeda
 
-//document.getElementById("currentSaldo").innerHTML = userbonus
+let currentSaldo = document.getElementById("currentSaldo")
+//currentSaldo.innerHTML = userbonus
    
+
 function buscar() {
 const inputbuscar = document.getElementById("inputbuscar")
 console.log(inputbuscar.value)
@@ -19,3 +22,53 @@ console.log(inputbuscar.value)
   
   
 }
+
+
+function CalcJuros() {
+ try {
+console.log("click do mouse")
+ClickMouseCalc.play()
+   
+let EMP = document.getElementById("EMP").value;
+const JurosSem = document.getElementById("jurosSem")
+const JurosProxSem = document.getElementById("jurosProxSem")
+const CalcTotal = document.getElementById("calcTotal")
+const ProxSem = document.getElementById("proximasem")
+const btncalc = document.getElementById("btnCalc")
+
+if(EMP == 0){
+    btncalc.style.opacity = "0.7"
+    btncalc.style.cursor = "not-allowed"
+    JurosSem.innerHTML = "Insira o valor na calculadora!"
+    JurosSem.style.color = "red"
+    CalcTotal.innerHTML = "Insira o valor na calculadora!"
+    CalcTotal.style.color = "red"
+    JurosProxSem.innerHTML = "Insira o valor na calculadora!"
+    JurosProxSem.style.color = "red"
+    ProxSem.innerHTML = "Insira o valor na calculadora!"
+    ProxSem.style.color = "red"
+    EMP.style.outlineColor = "red"
+  }else{
+  btncalc.style.opacity = "1"
+  btncalc.style.cursor = "pointer"
+  const porcent = 0.2 
+  const juros = EMP * porcent;
+  const total = (Number(EMP) + Number(juros));
+  const jurosNew = total * porcent
+  const TotalProxSem = total + jurosNew
+  
+  JurosSem.innerHTML = juros
+  CalcTotal.innerHTML = total
+  JurosProxSem.innerHTML = jurosNew
+  ProxSem.innerHTML = TotalProxSem
+  JurosSem.style.color = "green"
+  CalcTotal.style.color = "green"
+  JurosProxSem.style.color = "green"
+  ProxSem.style.color = "green"
+  EMP.style.outlineColor = "blue"}
+ }catch (err) {
+  console.log(err);
+}
+  
+   
+  }
--- js/functions.js
+++ js/functions.js
@@ -2,53 +2,7 @@ const WindowSongError1 = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41
 const alarm = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Shop empire 2 - Alarm.mp3?v=1660420687299.mp3');
 const ClickMouseFUNCTIONS = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/click%20do%20mouse.mp3?v=1661006466474');
 
-function CalcJuros() {
- try {
 
-   
-let EMP = document.getElementById("EMP").value;
-const JurosSem = document.getElementById("jurosSem")
-const JurosProxSem = document.getElementById("jurosProxSem")
-const CalcTotal = document.getElementById("calcTotal")
-const ProxSem = document.getElementById("proximasem")
-const btncalc = document.getElementById("btnCalc")
-
-if(EMP == 0){
-    btncalc.style.opacity = "0.7"
-    btncalc.style.cursor = "not-allowed"
-    JurosSem.innerHTML = "Insira o valor na calculadora!"
-    JurosSem.style.color = "red"
-    CalcTotal.innerHTML = "Insira o valor na calculadora!"
-    CalcTotal.style.color = "red"
-    JurosProxSem.innerHTML = "Insira o valor na calculadora!"
-    JurosProxSem.style.color = "red"
-    ProxSem.innerHTML = "Insira o valor na calculadora!"
-    ProxSem.style.color = "red"
-    EMP.style.outlineColor = "red"
-  }else{
-  btncalc.style.opacity = "1"
-  btncalc.style.cursor = "pointer"
-  const porcent = 0.2 
-  const juros = EMP * porcent;
-  const total = (Number(EMP) + Number(juros));
-  const jurosNew = total * porcent
-  const TotalProxSem = total + jurosNew
-  
-  JurosSem.innerHTML = juros
-  CalcTotal.innerHTML = total
-  JurosProxSem.innerHTML = jurosNew
-  ProxSem.innerHTML = TotalProxSem
-  JurosSem.style.color = "green"
-  CalcTotal.style.color = "green"
-  JurosProxSem.style.color = "green"
-  ProxSem.style.color = "green"
-  EMP.style.outlineColor = "blue"}
- }catch (err) {
-  console.log(err);
-}
-  
-   
-  }
 
 function mostrarsenha() {
   let inputSenha = document.getElementById('senha')
