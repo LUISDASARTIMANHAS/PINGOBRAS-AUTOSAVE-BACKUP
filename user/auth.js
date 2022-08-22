ae95bfb1536808ce4982791ba31216eccceb8b46
Glitch (pingobras) <none>
2022-08-22 14:50:37 +0000

    🎉🍦 Checkpoint ./user/auth.js:26026384/295

--- user/auth.js
+++ user/auth.js
@@ -4,15 +4,17 @@ let labelAlarm = document.querySelector("alert");
 
 if(token == "null"){
 let site = document.querySelector(".site-altura") 
-let head = document.querySelector("head") 
+
 const alarmAuto = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Shop empire 2 - Alarm.mp3?v=1660420687299.mp3');
 alert('Você precisa estar logado para acessar essa página')
 site.style.display = "none"
-head.style.display = "none"
 labelAlarm.style.display = "block";
 alarmAuto.play();
+  
 const myTimeout = setTimeout(REDIRECIONAR, 10000);
 function REDIRECIONAR() {window.location.href = "/login"}
 }else{
  localStorage.setItem( 'token',token);
 }
+let head = document.querySelector("nav") 
+head.style.display = "none"
