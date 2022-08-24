5525379250b9343dcd32f8c4af226d8a7412c1bb
Glitch (pingobras) <none>
2022-08-24 16:03:24 +0000

    🐸👨‍❤️‍💋‍👨 Checkpoint ./cadastro/STORAGEcad.js:26026384/1595

--- cadastro/STORAGEcad.js
+++ cadastro/STORAGEcad.js
@@ -37,3 +37,9 @@ UNIVERSALACCOUNT.push(
 )
 localStorage.setItem('listaUser', JSON.stringify(UNIVERSALACCOUNT))
 console.warn("STORAGECAD> Contas UNIVERSAL e ADMINISTRATIVAS Carregadas")}
+
+function RecarregarSTCAD() {
+ localStorage.removeItem('listaUser')
+console.warn("STORAGECAD> Contas UNIVERSAL e ADMINISTRATIVAS Carregadas")
+alert("O Banco de Dados Foi Recarregado")
+}
