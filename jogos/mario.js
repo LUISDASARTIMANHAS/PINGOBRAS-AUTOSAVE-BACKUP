f79cdba4e060bd71b82a45622c3560ef871ddf73
Glitch (pingobras) <none>
2022-09-07 20:15:16 +0000

    🏤🍙 Checkpoint ./jogos/mario.js:26026384/325

--- jogos/mario.js
+++ jogos/mario.js
@@ -1,5 +1,5 @@
 const mario = document.querySelector(".mario");
-
+const pipe = document.querySelector(".pipe");
 
 document.addEventListener("keydown", jump);
 function jump() {
@@ -13,5 +13,10 @@ function jump() {
 
 const loopPerdeu = setInterval(perdeu, 10)
 function perdeu() {
-  const pipePosition =  
+  const pipePosition =  pipe.offsetLeft;
+  if(pipePosition <= 120){
+    
+    pipe.style.animation = "none"
+    pipe.stile.left = `${pipePosition}px`
+  }
 }
