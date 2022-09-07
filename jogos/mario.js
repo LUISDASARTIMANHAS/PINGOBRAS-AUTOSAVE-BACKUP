b51265c4bc54eae0c7f69f9feea8ea001619e5c7
Glitch (pingobras) <none>
2022-09-07 23:46:57 +0000

    💼🏗 Checkpoint ./jogos/mario.js:26026384/355

--- jogos/mario.js
+++ jogos/mario.js
@@ -5,9 +5,10 @@ const telaWidth = screen.availWidth;
 const body = document.querySelector("body")
 const marioJump = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/jump%20super%20mario.mp3?v=1662590435349');
 const marioGameover= new Audio("https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Super Mario Bros. - Game Over Sound Effect.mp3?v=1662591735840");
+var HightScore = 15
 let placarGB = 0
 const labelPlacar = document.querySelector("#placar");
-const HightScore = document.querySelector()
+const LabelHightScore = document.querySelector("#HightScore")
 
 if(telaWidth <= 650){
   alert("A tela do usuario e muito curta!");
@@ -50,7 +51,12 @@ function perdeu() {
   }else{
     if(pipePosition < "-17" && marioPosition >= 80){
     placarGB = placarGB + 1;
-    labelPlacar.innerHTML = placarGB;}
+    labelPlacar.innerHTML = placarGB;} 
+  }
+  LabelHightScore.innerHTML = HightScore
+  
+  if(placarGB >= HightScore){
+    localStorage.setItem("Mario Jump HightScore", HightScore);
     
   }
 }
