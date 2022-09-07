c7c3bd03aa4bad1eff747ddf863ec0679cd03b88
Glitch (pingobras) <none>
2022-09-07 20:04:08 +0000

    💴🍰 Checkpoint ./jogos/mario.js:26026384/203 ./jogos/mario.html:26026384/26527

--- jogos/mario.js
+++ jogos/mario.js
@@ -1,7 +1,17 @@
-const mario = document.querySelector(".mario")
+const mario = document.querySelector(".mario");
 
 
 document.addEventListener("keydown", jump);
 function jump() {
-  mario.classList.add(".jump")
+  mario.classList.add("jump");
+  
+  setTimeout(() => {
+  mario.classList.remove("jump");
+  },500);
+  
+}
+
+const loopPerdeu = setInterval(perdeu, 10)
+function perdeu() {
+  const pipePosition =  
 }
