4705c6cb5c0cac2d2848d74abf9191f27bc04ad8
Glitch (pingobras) <none>
2022-09-12 19:21:32 +0000

    💼🚌 Checkpoint ./login/style.css:26026384/85 ./login/index.html:26026384/57 ./js/RGB.js:26026384/498

--- js/RGB.js
+++ js/RGB.js
@@ -17,51 +17,59 @@ RGB3.style.transition = "all 0.3s ease-out"
 CRGB1.style.transition = "all 0.3s ease-out"
 CRGB2.style.transition = "all 0.3s ease-out"
 
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function red() {
   RGB1.style.borderColor = "red"
   RGB2.style.borderColor = "red"
   CRGB1.style.color = "red"
-  CRGB2.style.color = "red"
+  CRGB2.style.color = "red"}
 }
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function orange() {
   RGB1.style.borderColor = "orange"
   RGB2.style.borderColor = "orange"
   CRGB1.style.color = "orange"
-  CRGB2.style.color = "orange"
+  CRGB2.style.color = "orange"}
 }
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function yellow() {
   RGB1.style.borderColor = "yellow"
   RGB2.style.borderColor = "yellow"
   CRGB1.style.color = "yellow"
-  CRGB2.style.color = "yellow"
+  CRGB2.style.color = "yellow"}
 }
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function green() {
   RGB1.style.borderColor = "green"
   RGB2.style.borderColor = "green"
   CRGB1.style.color = "green"
-  CRGB2.style.color = "green"
+  CRGB2.style.color = "green"}
 }
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function blue() {
   RGB1.style.borderColor = "blue"
   RGB2.style.borderColor = "blue"
   CRGB1.style.color = "blue"
-  CRGB2.style.color = "blue"
+  CRGB2.style.color = "blue"}
 }
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function darkblue() {
   RGB1.style.borderColor = "darkblue"
   RGB2.style.borderColor = "darkblue"
   CRGB1.style.color = "darkblue"
-  CRGB2.style.color = "darkblue"
+  CRGB2.style.color = "darkblue"}
 }
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function lightblue() {
   RGB1.style.borderColor = "lightblue"
   RGB2.style.borderColor = "lightblue"
   CRGB1.style.color = "lightblue"
-  CRGB2.style.color = "lightblue"
+  CRGB2.style.color = "lightblue"}
 }
+if(!RGB1 || !RGB2 || !RGB3 || !CRGB1 || !CRGB2 ){
 function purple() {
   RGB1.style.borderColor = "purple"
   RGB2.style.borderColor = "purple"
   CRGB1.style.color = "purple"
-  CRGB2.style.color = "purple"
+  CRGB2.style.color = "purple"}
 }
