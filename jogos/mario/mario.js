0086081ab3bc56aed2827b605d8d9732c9a28371
Glitch (pingobras) <none>
2022-09-13 12:01:09 +0000

    👨‍👨‍👦⛽️ Checkpoint ./jogos/mario/mario.js:26026384/1

--- jogos/mario/mario.js
+++ jogos/mario/mario.js
@@ -13,7 +13,7 @@ const labelTela = document.querySelector("#labelTela");
 const body = document.querySelector("body");
 const marioJump = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/jump%20super%20mario.mp3?v=1662590435349');
 const marioGameover= new Audio("https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Super Mario Bros. - Game Over Sound Effect.mp3?v=1662591735840");
-let placarLocal = 0
+let placarLocal = 0;
 var second = 0;
 const labelPlacar = document.querySelector("#placar");
 const LabelHightScore = document.querySelector("#HightScore");
