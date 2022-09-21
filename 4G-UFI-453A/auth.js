fc02200774588d0740844e1a3003d73bf3219d44
Glitch (pingobras) <none>
2022-09-21 10:37:34 +0000

    🐒😄 Checkpoint ./4G-UFI-453A/auth.js:26026384/449

--- 4G-UFI-453A/auth.js
+++ 4G-UFI-453A/auth.js
@@ -1,10 +1,16 @@
-const body = document.querySelector = "body"
-const Registro = localStorage.getItem("Registo4G");
+const body = document.querySelector("body")
+const STRegistro = localStorage.getItem("Registo4G");
+body.style.display = "block"
+
+if(STRegistro == "null"||STRegistro == null){
+  const usercadInp = prompt("Cadastre seu usuario","Meu Nome") 
+  
+}else{
 const userinput = prompt("Insira Seu Usuario!","Administrador")
-    
-if(Registro == userinput) {
+/*if(STRegistro == userinput) {
 body.style.display = "block"    
 }else{
 alert("Não Foi Possivel Logar Tente Novamente!")
 window.location.href = "https://pingobras.glitch.me/4G-UFI-453A"
-    }
+    }*/
+}
