108bbd6a56d7086e01e209414d12f97db69648ab
Glitch (pingobras) <none>
2022-09-22 16:56:47 +0000

    🛫📰 Checkpoint ./4G-UFI-453A/auth.js:26026384/252 ./4G-UFI-453A/index.html:26026384/183

--- 4G-UFI-453A/auth.js
+++ 4G-UFI-453A/auth.js
@@ -1,16 +1,16 @@
 const body = document.querySelector("body")
 const STRegistro = localStorage.getItem("Registo4G");
-body.style.display = "block"
+const labelUserWifi = document.getElementById("userWifi");
 
 if(STRegistro == "null"||STRegistro == null){
   const usercadInp = prompt("Cadastre seu usuario","Meu Nome") 
-  
+  localStorage.setItem("Registo4G",usercadInp)
 }else{
 const userinput = prompt("Insira Seu Usuario!","Administrador")
-/*if(STRegistro == userinput) {
+if(STRegistro == userinput) {
 body.style.display = "block"    
 }else{
 alert("Não Foi Possivel Logar Tente Novamente!")
 window.location.href = "https://pingobras.glitch.me/4G-UFI-453A"
-    }*/
+    }
 }
