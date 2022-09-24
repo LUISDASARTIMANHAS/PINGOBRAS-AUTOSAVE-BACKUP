79560656ee9d84174a5cdcaa4cdf9c15c6851ca4
Glitch (pingobras) <none>
2022-09-24 13:36:38 +0000

    🍎🍐 Checkpoint ./GlobalDB.js:26026384/10

--- GlobalDB.js
+++ GlobalDB.js
@@ -7,12 +7,12 @@ let db = [
 {"nomeCad": "Florisvaldo de Oliveira Schulz", 
  "userCad": "Valdo", 
  "senhaCad": "96451290", 
- "saldoCad": 2.00},
+ "saldoCad": 2},
 
 {"nomeCad": "Luis Augusto de Souza Carvalho", 
  "userCad": "LUIS DAS ARTIMANHAS", 
  "senhaCad": "998774163", 
- "saldoCad": 3.50},
+ "saldoCad": 4},
 
 {"nomeCad": "José Eduardo Batista de Souza", 
  "userCad": "notfound", 
@@ -22,7 +22,7 @@ let db = [
 {"nomeCad": "Diogo Antonio Nienke Batista", 
  "userCad": "did zin", 
  "senhaCad": "z0mbieltr2", 
- "saldoCad": 1} 
+ "saldoCad": 5} 
 ]
 const dbString = JSON.stringify(db);
 localStorage.setItem("GlobalDB",dbString)
