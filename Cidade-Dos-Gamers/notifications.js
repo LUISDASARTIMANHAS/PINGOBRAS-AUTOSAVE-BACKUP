330a13edd75275101caeb4cb6337cb86f4c5a4ac
Glitch (pingobras) <none>
2022-10-09 21:29:50 +0000

    🤑💘 Checkpoint ./Cidade-Dos-Gamers/notifications.js:26026384/882 ./Cidade-Dos-Gamers/index.html:26026384/1475

--- Cidade-Dos-Gamers/notifications.js
+++ Cidade-Dos-Gamers/notifications.js
@@ -2,14 +2,7 @@
   if (!("Notification" in window)) {
     alert("Este browser não suporta notificações de Desktop");
   }
-    
-    document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState === 'visible') {
-      // The tab has become visible so clear the now-stale Notification.
-     n.close(); 
-    }
-  });
-    
+
       var opcoes = {
       body: "Ainda não a recrutamento de Staffs!",
       icon: "https://cdn.discordapp.com/attachments/1022972920517099580/1026201314457296956/Picsart_22-09-27_22-49-32-293.jpg",
@@ -36,10 +29,18 @@
         var notification = new Notification("As notificações foram ativadas!");}});
   }
 }
-    
+  
     
 if (Notification.permission === "granted") {
   var n = new Notification("Recrutamento De Staffs",opcoes);
+  
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'visible') {
+      // The tab has become visible so clear the now-stale Notification.
+     n.close(); }});
+}
+
+if (Notification.permission === "granted") {
   var EventoNotify = new Notification("Evento novo no servidor",Evento);
   
 document.addEventListener('visibilitychange', () => {
