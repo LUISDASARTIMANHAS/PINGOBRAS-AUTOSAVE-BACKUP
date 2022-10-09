cc71c5ff986c6fc5eb2bb1913f6a9bb5f28018d5
Glitch (pingobras) <none>
2022-10-09 21:41:45 +0000

    🏑🍏 Checkpoint ./Cidade-Dos-Gamers/notifications.js:26026384/1693

--- Cidade-Dos-Gamers/notifications.js
+++ Cidade-Dos-Gamers/notifications.js
@@ -1,15 +1,26 @@
 // Verifica se o browser suporta notificações
   if (!("Notification" in window)) {
     alert("Este browser não suporta notificações de Desktop");
+  }else{
+  // Verificando suporte a tecnologia
+  if (window.webkitNotifications) {
+  console.log('Seu browser suporta Notifications');
+}else {
+  console.warn('Seu browser não suporta Notifications =(');
+}
+if (Notification.permission === 'denied') {
+    alert('Usuário não deu permissão para notificações');
   }
 
+    
+  }
       var opcoes = {
       body: "Ainda não a recrutamento de Staffs!",
       icon: "https://cdn.discordapp.com/attachments/1022972920517099580/1026201314457296956/Picsart_22-09-27_22-49-32-293.jpg",
       silent: false,
       vibrate: [200, 100, 200],
       renotify: true,
-      tag: "song"
+      tag: 'Evento',
   }
   var Evento = {
       body: "Terá um evento, onde a pessoa que enviar mais mensagems no servidor e interagir, irá ganhar uma certa quantia de Sonhos e um cargos específico também, esse é o nosso primeiro evento da Cidade dos Gamers.",
