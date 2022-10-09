681ca28a898c2b16fddcb1a521d101cfc91e0b2b
Glitch (pingobras) <none>
2022-10-09 21:17:43 +0000

    👨‍❤️‍💋‍👨💫 Checkpoint ./Cidade-Dos-Gamers/style.css:26026384/72 ./Cidade-Dos-Gamers/index.html:26026384/261 ./Cidade-Dos-Gamers/notifications.js:26026384/682

--- Cidade-Dos-Gamers/notifications.js
+++ Cidade-Dos-Gamers/notifications.js
@@ -18,6 +18,14 @@
       renotify: true,
       tag: "song"
   }
+  var Evento = {
+      body: "Terá um evento, onde a pessoa que enviar mais mensagems no servidor e interagir, irá ganhar uma certa quantia de Sonhos e um cargos específico também, esse é o nosso primeiro evento da Cidade dos Gamers.",
+      icon: "https://cdn.discordapp.com/attachments/1022972920517099580/1026201314457296956/Picsart_22-09-27_22-49-32-293.jpg",
+      silent: false,
+      vibrate: [200, 100, 200],
+      renotify: true,
+      tag: "song"
+  }
     
   function notifyMe() {
   // Otherwise, we need to ask the user for permission
@@ -32,6 +40,7 @@
     
 if (Notification.permission === "granted") {
   var n = new Notification("Recrutamento De Staffs",opcoes);
+  var EventoNotify = new Notification("Evento novo no servidor",Evento);
   
 document.addEventListener('visibilitychange', () => {
   if (document.visibilityState === 'visible') {
