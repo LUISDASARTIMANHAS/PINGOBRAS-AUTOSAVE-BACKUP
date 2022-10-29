f998764d88f790eaacea9dbe7b12a9ff3c18d9cd
Glitch (pingobras) <none>
2022-10-29 14:18:05 +0000

    🙏🚍 Checkpoint ./carteira/index.html:26026384/318 ./carteira/painel-resgate.js:26026384/148 ./css/style.css:26026384/1002

--- carteira/painel-resgate.js
+++ carteira/painel-resgate.js
@@ -2,22 +2,21 @@ const siteAltura = document.querySelector(".site-altura");
 const btn = document.querySelector("button");
 const a = document.querySelector("a");
 const fechar = document.getElementById("close");
-const popup = document.getElementById("popup");
+const painel = document.getElementById("painel");
 
     
-function (){
+function painelResgate(){
   
-if(!popup){
+if(!painel){
   alert("Não Foi Posivel Conectar-Se Ao Servidor! ERROR: 500 INTERNAL SERVER ERROR");
 }
   
-popup.style.display = "block";
+painel.style.display = "block";
 siteAltura.style.opacity = "0.2";
 siteAltura.style.cursor = "not-allowed";
 btn.style.display = "none"
 a.style.display = "none"
-console.log("LOG> ⚠️Anuncio carregado!");
-alert("Não a anúncios disponíveis!");
+console.log("LOG> ⚠️Painel carregado!");
 }  
 
 
@@ -25,10 +24,10 @@ if(!fechar) {
 console.warn("Deu Pau Ao Fechar Painel De Resgate Vale")
 }else{
   fechar.addEventListener("click", function(){
-    popup.style.display = "none";
+    painel.style.display = "none";
     siteAltura.style.opacity = "1";
     siteAltura.style.cursor = "default";
-    btn.style.display = "block"
-    a.style.display = "block"
-  console.log("LOG> ⚠️Anuncio fechado!");
+    btn.style.display = ""
+    a.style.display = ""
+  console.log("LOG> ⚠️painel fechado!");
 });}
