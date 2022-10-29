ca3b21a90abd2c7976ba803b2cde94745bc317fb
Glitch (pingobras) <none>
2022-10-29 15:16:14 +0000

    🐀🍓 Checkpoint ./carteira/verify-vale.js:26026384/2077 ./carteira/index.html:26026384/3674 ./calcs&dividendos.html:26026384/7

---
+++ carteira/verify-vale.js
@@ -0,0 +1,16 @@
+const loopVale = setInterval(refreshVerifyVale, 1000);
+
+function refreshVerifyVale() {
+const ClickMouseCalc = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/click%20do%20mouse.mp3?v=1661006466474');
+   
+ try {
+const EMPstyle = document.getElementById("presente-code");
+const msgError = document
+
+ }catch (err) {
+  console.log(err);
+}
+  
+   
+  
+}//final do repetidor
