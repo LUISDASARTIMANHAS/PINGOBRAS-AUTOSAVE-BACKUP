d7241c6d496d02c6026459ff16495d1e173c987e
Glitch (pingobras) <none>
2022-10-29 15:40:14 +0000

    🖖🌕 Checkpoint ./carteira/verify-vale.js:26026384/306 ./carteira/index.html:26026384/14

--- carteira/verify-vale.js
+++ carteira/verify-vale.js
@@ -3,8 +3,9 @@ const loopVale = setInterval(refreshVerifyVale, 1000);
 function refreshVerifyVale() {
 const ClickMouseCalc = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/click%20do%20mouse.mp3?v=1661006466474');
 const btnResgatarVale = document.getElementById("btn-resgatar-vale");
+const span = document.getElementById("spanVale");
 let btnRVStyle = btnResgatarVale.style;  
-  
+ 
  try {
 const code = document.getElementById("presente-code");
 const msgError = document.getElementById("msgError");
@@ -25,7 +26,8 @@ msgError.innerHTML = "O CODIGO NÃO E VALIDO!"
  }catch (err) {
   console.log(err);
   btnRVStyle.cursor="not-allowed"
-  btnRVStyle.cursor="not-allowed"
+  btnRVStyle.background="red"
+  span.innerHTML="Indisponivel"
 }
   
    
