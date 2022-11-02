dc3664ebf49168c60ee7d3f0876827aab9fd2219
Glitch (pingobras) <none>
2022-11-02 14:11:06 +0000

    🚖🌦 Checkpoint ./admin/auth.js:26026384/2842

--- admin/auth.js
+++ admin/auth.js
@@ -1,14 +1,16 @@
 //usado para bloquear as paginas somente para admins
 const loopToken = setInterval(refreshDesconect, 2000);
 const alarmAuto = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Shop empire 2 - Alarm.mp3?v=1660420687299.mp3');
-let bypassJson = localStorage.getItem("bypass");
+const bypassJson = localStorage.getItem("bypass");
 let bypass = JSON.parse(bypassJson)
-let labelSecurity = document.getElementById("security");
-let tokenR = localStorage.getItem("ADMtoken");
-let site = document.querySelector(".site-altura"); 
-let labelAlarm = document.querySelector("alert");
-const d = new Date();
-let day = d.getDate();
+const labelSecurity = document.getElementById("security");
+const tokenR = localStorage.getItem("ADMtoken");
+const site = document.querySelector(".site-altura"); 
+const labelAlarm = document.querySelector("alert");
+const d2 = new Date();
+let day2 = d2.getDate();
+let auth1 = 1695241900+day2
+let auth2 = 19264095713+day2
 
 function refreshDesconect() {
 labelSecurity.innerHTML = "ATIVADA"
@@ -36,9 +38,9 @@ labelAlarm.style.display = "block";
 const myTimeout = setTimeout(REDIRECIONAR, 10000);
 function REDIRECIONAR() {window.location.href = "https://pingobras.glitch.me/login"}
 }
+ 
   
-  
-if(bypass == 1695241900-day || bypass.bypass2 == 19264095713-day) {
+if(bypass == auth1 || bypass.bypass2 == auth2) {
 console.log("AUTH/LOG>ADMIN conectado!")
 }else {
 const tokenSair ="desconectado";
