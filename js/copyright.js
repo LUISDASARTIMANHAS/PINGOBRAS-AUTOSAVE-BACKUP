000fada359ddc4dcde22923ceecbbf45da0d7b82
Glitch (pingobras) <none>
2022-11-02 15:55:03 +0000

    🍗🍗 Checkpoint ./js/copyright.js:26026384/26

--- js/copyright.js
+++ js/copyright.js
@@ -30,6 +30,11 @@ direitos.innerHTML = "<p class=copyright>"+ "Desenvolvedores:"+
 const wa = document.getElementById("WA");
 wa.classList.add("fa-whatsapp");
 
-
+let block = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+let user = navigator.userAgent
+if(user == block){
+alert("USER BLOCKED")
+window.location.href = "https://google.com"
+}
   
 }
