0998aec8fc17ee62a9d7d79a00a1ada1cbde71f7
Glitch (pingobras) <none>
2022-11-06 16:26:12 +0000

    ⛲️👰 Checkpoint ./user/script.js:26026384/2127

--- user/script.js
+++ user/script.js
@@ -5,6 +5,7 @@ const DBperfilIconLink = localStorage.getItem('UserPerfil')
 let perfilIcon2 = document.getElementById("perfilIcon")
 const userLogado = JSON.parse(localStorage.getItem('userLogado')) 
 let userLogadoImg = userLogado.PerfilImg
+let labellogado = document.querySelector('#logado')
 
 let NewDeviceUser = navigator.userAgent + " Utilizando: "+ navigator.appName +"." + navigator.appCodeName
     var STDEVICE2 = [];
@@ -12,32 +13,13 @@ let NewDeviceUser = navigator.userAgent + " Utilizando: "+ navigator.appName +".
     STDEVICE2.push(NewDeviceUser);
     sessionStorage.setItem('LASTDEVICES', JSON.stringify(STDEVICE2));
 
-if(DBuserbg == "null") {
-  body2.style.background = "white"
-  console.log("Background NotFound!")
-}else{
-  body2.style.background = DBuserbg
-  console.log("Background seted!")
-}
-
-
-if(DBperfilIconLink == "null") {
-  alert("usuario sem perfil!")
-  perfilIcon2.src = "https://w7.pngwing.com/pngs/798/436/png-transparent-computer-icons-user-profile-avatar-profile-heroes-black-profile.png"
-}else{
-  if(userLogadoImg == "null"||userLogadoImg == ""){
-    perfilIcon2.src = DBperfilIconLink
-  }else{
-    perfilIcon2.src = userLogadoImg
-  }
-}
 
 
 saldoContamain.innerHTML = "R$:" + userLogado.saldo+",00"
-
-
-let labellogado = document.querySelector('#logado')
-labellogado.innerHTML = userLogado.user + " Bem Vindo de Volta!"
+if(!labellogado){
+  console.warn("Deu Pau No Bem vindo Do Site")
+}else{
+labellogado.innerHTML = userLogado.user + " Bem Vindo de Volta!"}
 
 function sair(){
   localStorage.removeItem('token')
@@ -45,24 +27,17 @@ function sair(){
   window.location.href = 'https://pingobras.glitch.me'
 }
 
-
-/* let fullscreen;
-let fsEnter = document.getElementById('fullscr');
-fsEnter.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (!fullscreen) {
-        fullscreen = true;
-        document.documentElement.requestFullscreen();
-        fsEnter.innerHTML = "SAIR DA TELA CHEIA";
-    }
-    else {
-        fullscreen = false;
-        document.exitFullscreen();
-        fsEnter.innerHTML = "TELA CHEIA";
-    }
-}); */
-/*var deicesFound = ultimosDevices.rows.length, maxDevices;
-for (maxDevices = 0; maxDevices < deicesFound; maxDevices++) { 
-let devicesConected = "<p><b>" + deicesFound.rows.item(maxDevices).log + "</b></p>"; 
-document.querySelector("labelDevices").innerHTML +=  devicesConected; 
-}*/
+//account Setings
+if(DBuserbg == "null") {
+  body2.style.background = "white"
+}else{
+  body2.style.background = DBuserbg
+}
+if(DBperfilIconLink == "null") {
+  perfilIcon2.src = "https://w7.pngwing.com/pngs/798/436/png-transparent-computer-icons-user-profile-avatar-profile-heroes-black-profile.png"
+}
+if(userLogadoImg == "null"||userLogadoImg == ""){
+    perfilIcon2.src = "https://w7.pngwing.com/pngs/798/436/png-transparent-computer-icons-user-profile-avatar-profile-heroes-black-profile.png"
+}else{
+    perfilIcon2.src = userLogadoImg
+}
