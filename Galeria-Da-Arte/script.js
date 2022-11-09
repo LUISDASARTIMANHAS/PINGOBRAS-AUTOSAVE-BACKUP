9f39c7132ba2d2c6f4f4bc9a181b8cb29be4f9d9
Glitch (pingobras) <none>
2022-11-09 14:58:12 +0000

    🍄⏱ Checkpoint ./Galeria-Da-Arte/script.js:26026384/600 ./Galeria-Da-Arte/diogo.html:26026384/6

--- Galeria-Da-Arte/script.js
+++ Galeria-Da-Arte/script.js
@@ -1,29 +1,32 @@
 const bar = document.getElementById("bar");
 const DBRLabel = document.getElementById("DBRLabel");
-let DBUsado = 18
-const DBPercent = DBUsado/100;
+let DBUsado = 98;
+const DBrestante = 500-DBUsado/100*10;
 
-let DBrestante = 500-DBPercent;
+
 var width = 0;
 var id = setInterval(frame, 100);
-
 
 function frame() {
 if(width >= 12){
 bar.style.background = "blue"
-DBRLabel
+DBRLabel.style.background = "blue"
 }
 if(width >= 25){
 bar.style.background = "yellow"
+DBRLabel.style.background = "yellow"
 }
 if(width >= 50){
 bar.style.background = "orange"
+DBRLabel.style.background = "orange"
 }
 if(width >= 75){
 bar.style.background = "red"
+DBRLabel.style.background = "red"
 }
 if(width >= 99){
 bar.style.background = "purple"
+DBRLabel.style.background = "purple"
 }
   
 if (width >= DBUsado) {
