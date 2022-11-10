3e28fd94814fb3882d494a67c0e55055b06f5ebb
Glitch (pingobras) <none>
2022-11-10 23:50:29 +0000

    ✋🌶 Checkpoint ./.gitignore:26026384/190

--- cadastro/STORAGEcad.js
+++
@@ -1,5 +0,0 @@
-function RecarregarSTCAD() {
- localStorage.removeItem('listaUser')
-console.warn("STORAGECAD> Contas UNIVERSAL e ADMINISTRATIVAS Carregadas")
-alert("O Banco de Dados Foi Recarregado")
-}
