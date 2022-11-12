0f22b9882df2f1f13afa24a0a583679c3a974652
Glitch (pingobras) <none>
2022-11-12 20:18:09 +0000

    👚🚇 Checkpoint ./bingo/sorteador.js:26026384/430

--- bingo/sorteador.js
+++ bingo/sorteador.js
@@ -7,16 +7,14 @@ var QBolas = 0
 
 setInterval(Sorteador,2000)  
 function Sorteador(){
-  var divs = document.querySelectorAll('span'), i;
-  if(QBolas => 50){
-          console.log(QBolas)
-        clearInterval(Sorteador)
-        LabelNumSort.innerHTML = "Chega De Sortear Bolas!"}
+  if(QBolas == 50){
+      console.log(QBolas)
+      clearInterval(Sorteador)
+      LabelNumSort.innerHTML = "Chega De Sortear Bolas!"
 }else{
+  var divs = document.querySelectorAll('span'), i;
   numAleatorio = Math.floor(Math.random() * 90)
   QBolas = QBolas+1
-  
-  
   LabelNumTSort.innerHTML = QBolas+"/50"
   LabelNumSort.innerHTML = numAleatorio
   LabelQNum.innerHTML =  divs.length
@@ -24,6 +22,6 @@ function Sorteador(){
     for (i = 0; i < divs.length; ++i) {
       if(divs[i].innerText == numAleatorio){
       	divs[i].style.color = "red";}	
-    }
+    }}
         
 }//fim do sorteador
