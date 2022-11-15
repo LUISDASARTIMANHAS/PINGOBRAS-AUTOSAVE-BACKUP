f80040ddf2df9758f83e8c3ec11ffa2d1e4bd7e5
Glitch (pingobras) <none>
2022-11-15 16:37:26 +0000

    🖐🙈 Checkpoint ./js/functions.js:26026384/3697

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,13 @@
 const WindowSongError1 = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Windows-error-song?v=1656019161212.mp3?v=1651870846885.mp3');
 const alarm = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Shop empire 2 - Alarm.mp3?v=1660420687299.mp3');
 const ClickMouseFUNCTIONS = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/click%20do%20mouse.mp3?v=1661006466474');
-
-
+const Subir = document.getElementById("back-to-top");
+const Descer = document.getElementById("jsDescer");
+const btnListDev = document.getElementById("btnlist");
+const listDEV = document.getElementById("listDevedores");
+let btnlistChangelogs = document.getElementById('btnlistChangelogs');
+var listChangelogs = document.getElementById('listChangelogs');
+const btnALL = document.querySelectorAll("button");
 
 function mostrarsenha() {
   let inputSenha = document.getElementById('senha')
@@ -15,46 +20,7 @@ function mostrarsenha() {
     inputSenha.setAttribute('type', 'password')
     inputSenha.placeholder = "****"}
   
-}
-
-function openForm() {
-  ClickMouseFUNCTIONS.play();
-  document.getElementById("jschat").style.display = "block";
-  alert("ESSA FUNÇÃO E EXPERIMENTAL E PODE NÃO FUNCIONAR OU CONTER BUGS!");
-  console.log("FUNCTIONS/LOG> O chat foi aberto!");
-  WindowSongError1.play();
-setInterval(autopauseWindowSongError1, 15000);
-function autopauseWindowSongError1() {
-  WindowSongError1.pause() }
-
-}
-function closeForm() {
-  ClickMouseFUNCTIONS.play();
-  document.getElementById("jschat").style.display = "none";
-  console.log("FUNCTIONS/LOG> O chat foi fechado");
-}
-
-var Subir = document.querySelector("#back-to-top");
-if(!Subir) {
-console.warn("Deu Pau No Redirect Para Subir Do Site")
-}else{
-Subir.addEventListener("click", function() {
-  ClickMouseFUNCTIONS.play();
-    window.scrollTo(0, 0);
-  console.log("FUNCTIONS/LOG> O Usuario foi redirecionado para cima!");
-});
-}
-var Descer = document.querySelector("#jsDescer");
-
-if(!Descer) {
-console.warn("Deu Pau No Redirect Para Descer Do Site")
-}else{
-Descer.addEventListener("click", function() {
-  ClickMouseFUNCTIONS.play();
-    window.scrollTo(0, 2000);
-  console.log("FUNCTIONS/LOG> O Usuario foi redirecionado para baixo!");});
-}
-  
+} 
   
 function pageFilmSerie() {
 ClickMouseFUNCTIONS.play();
@@ -72,57 +38,34 @@ function REDIRECIONAR() {window.location.href = "https://pingobras.glitch.me"}
 localStorage.setItem("bypass","0")
 }
 
-
-const music = new Audio('https://cdn.glitch.global/b39d6a4a-0e14-4b41-930d-29d3ccd6c137/Electro-Light%20-%20Symbolism%20%5BNCS%20Release%5D.mp3?v=1651870846885.mp3');
-function play() {
-music.play();
-music.loop =true;}
-
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
-}
-
-let btnlist = document.getElementById('btnlist');
-var listDEV = document.getElementById('listDevedores');
-if(!btnlist || !listDEV) {
+if(!btnListDev || !listDEV) {
 console.warn("Deu Pau Nos Dados Dos Devedores Do Site")
 }else{
-btnlist.addEventListener('click', function() {
+btnListDev.addEventListener('click', function() {
   ClickMouseFUNCTIONS.play();
-    if(listDEV.style.display === 'block') {
-      listDEV.style.display = 'none';
-  } else {
-      listDEV.style.display = 'block';
-  }
+if(listDEV.style.display === 'block') {listDEV.style.display = 'none';
+}else{listDEV.style.display = 'block';}
 });
-btnlist.addEventListener('mouseover', function() {
+btnListDev.addEventListener('mouseover', function() {
 ClickMouseFUNCTIONS.play();
-listDEV.style.display = 'block';
-})
+listDEV.style.display = 'block';})
 }
 
 function pausePingobras(){
 const music = document.getElementById('pingobras-music');
-music.pause()
-  
-}
+music.pause();}
 
 
 //Events listener
-let btnlistChangelogs = document.getElementById('btnlistChangelogs');
-var listChangelogs = document.getElementById('listChangelogs');
-
 if(!btnlistChangelogs || !listChangelogs) {
 console.warn("Deu Pau Nos Dados Da Changelogs Do Site")
 }else{
-  
 btnlistChangelogs.addEventListener('click', function() {
 ClickMouseFUNCTIONS.play();
-    if(listChangelogs.style.display === 'block') {
+if(listChangelogs.style.display === 'block') {
       listChangelogs.style.display = 'none';
       console.log("FUNCTIONS/LOG> A Changelogs foi escondida!");
-  }else{
+}else{
       listChangelogs.style.display = 'block';
     console.log("FUNCTIONS/LOG> A Changelogs esta visivel!")};});
   
@@ -132,11 +75,23 @@ listChangelogs.style.display = 'block';
     console.log("FUNCTIONS/LOG> A Changelogs esta visivel!");})
 }
 
-var batepapoDescer = document.querySelector("#jsDescerBatePapo");
-if(!batepapoDescer) {
- console.warn("Deu Pau No Bate-Papo do Site!")  
+
+if(!Subir) {
+console.warn("Deu Pau No Redirect Para Subir Do Site")
 }else{
-batepapoDescer.addEventListener("click", function() {
-    window.scrollTo(0, 2000);
-  console.log("FUNCTIONS/LOG> O Usuario foi redirecionado para bate-papo!");});
+Subir.addEventListener("click", function() {
+  ClickMouseFUNCTIONS.play();
+    window.scrollTo(0, 0);
+  console.log("FUNCTIONS/LOG> O Usuario foi redirecionado para cima!");});
 }
+if(!Descer) {
+console.warn("Deu Pau No Redirect Para Descer Do Site")
+}else{
+Descer.addEventListener("click", function() {
+  ClickMouseFUNCTIONS.play();
+    window.scrollTo(0, 2000);
+  console.log("FUNCTIONS/LOG> O Usuario foi redirecionado para baixo!");});
+}
+
+
+//songs
