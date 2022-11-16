f5f186e0a7190413c86606209114c8337c442e8d
Glitch (pingobras) <none>
2022-11-16 10:47:40 +0000

    💅🌓 Checkpoint ./Galeria-Da-Arte/diogo.html:26026384/105 ./Galeria-Da-Arte/sddo.js:26026384/820

--- Galeria-Da-Arte/sddo.js
+++ Galeria-Da-Arte/sddo.js
@@ -1,20 +1,24 @@
-function authLDA(){
+const body = document.querySelector("body");
+body.style.marginTop = "0%"
 let senhaSite = "qweasdzxc124"
 const main = document.getElementById("main");
 const login = document.getElementById("screenLoad");
 const labelLogin = document.getElementById("labelLogin");
 const loadingBar = document.getElementById("loadingBar");
+
+
+function authLDA(){
 const nav = document.querySelector("nav");
 let mainSty = main.style
 let loginSty = login.style
 const userinput = document.getElementById("userInput").value
-  
+
+
     var iniciar2 = "no";
     var barload = document.getElementById("barlogin");
     var width = 50;
     var barspeed = 200;
     var id = setInterval(frame, barspeed);
-
 
 function frame() {
       if (width >= 100) {
@@ -25,6 +29,7 @@ if(senhaSite == userinput) {
       loginSty.display = "none"
       mainSty.display = ""
       nav.style.display = ""
+      body.style.marginTop = "20%"
 }else{
       labelLogin.innerHTML = "NÃO FOI POSSIVEL FAZER LOGIN⚠️"}
 }else{
