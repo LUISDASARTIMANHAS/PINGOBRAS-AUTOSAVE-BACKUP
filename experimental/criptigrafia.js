514126a83635b7f62a80069a57f605828dc8b781
Glitch (pingobras) <none>
2022-11-19 14:12:33 +0000

    🐀🛩 Checkpoint ./experimental/criptigrafia.js:26026384/830

--- experimental/criptigrafia.js
+++ experimental/criptigrafia.js
@@ -1,14 +1,19 @@
 const loopCripto = setInterval(refreshCripto,1000)
 
 function refreshCripto(){
-let AR = value.replaceAll("A",0)
-let BR = AR.replaceAll("B",1)
-let CR = BR.replaceAll("E",3)
-const A = 0
-const B = 1
-const C = 2
-const D = 3
-const E = 4
+const InpASerCripto = document.getElementById("msmASerCripto").value;
+const Criptografar = InpASerCripto.toUpperCase()
+const AR = Criptografar.replaceAll("A",0)
+const BR = AR.replaceAll("B",1)
+const CR = BR.replaceAll("C",2)
+const DR = CR.replaceAll("C",3)
+const ER = DR.replaceAll("E",4)
+const FR = ER.replaceAll("F",5)
+const GR = FR.replaceAll("G",6)
+const HR = GR.replaceAll("H",7)
+const IR = HR.replaceAll("I",8)
+const JR = IR.replaceAll("J",9)
+const KR = JR.replaceAll("K",1+"x")
 const F = 5
 const G =6
 const H =7
@@ -31,9 +36,9 @@ const X = 5+"y"
 const Y = 6+"y"
 const Z =  7+"y"
 const _ = " "
-const InpASerCripto = document.getElementById("msmASerCripto").value;
+
 const msmC = document.getElementById("msmCripto");
-let phrase = I+_+L+O+V+E+_+Y+O+U+_+InpASerCripto;
+let phrase = ER;
 
 msmC.innerHTML = phrase
 
