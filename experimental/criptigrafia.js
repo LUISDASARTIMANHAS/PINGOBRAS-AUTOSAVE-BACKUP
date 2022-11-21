95f13d904347122f12dac7ce55325d7bbc999a20
Glitch (pingobras) <none>
2022-11-21 19:22:42 +0000

    🎽🐛 Checkpoint ./experimental/criptigrafia.js:26026384/1132

--- experimental/criptigrafia.js
+++ experimental/criptigrafia.js
@@ -34,5 +34,39 @@ const ReplacedItems = ZR
 const msmC = document.getElementById("msmCripto");
 
 msmC.innerHTML = ReplacedItems
+//Fim do criptografo
+  
+  let InpASerDescripto = document.getElementById("msmASerDescripto").value;
+let Criptografar2 = InpASerDescripto.toUpperCase()
+let AD = Criptografar.replaceAll(0,"A")
+let BD = AD.replaceAll(1,"B")
+let CD = BD.replaceAll(2,"C")
+let DD = CD.replaceAll(3,"D")
+let ED = DD.replaceAll(4,"E")
+let FD = ED.replaceAll(5,"F")
+let GD = FD.replaceAll(6,"G")
+let HD = GD.replaceAll(7,"H")
+let ID = HD.replaceAll("I",8)
+let JD = ID.replaceAll("J",9)
+let KD = JD.replaceAll("K",1+"x")
+let LD = KD.replaceAll("L",2+"x")
+let MD = LD.replaceAll("M",3+"x")
+let ND = MD.replaceAll("N",4+"x")
+let OD = ND.replaceAll("O",5+"x")
+let PD = OD.replaceAll("P",6+"x")
+let QD = PD.replaceAll("Q",7+"x")
+let RD = QD.replaceAll("R",8+"x")
+let SD = RD.replaceAll("S",9+"x")
+let TD = SD.replaceAll("T",1+"y")
+let UD = TD.replaceAll("U",2+"y")
+let VD = UD.replaceAll("V",3+"y")
+let WD = VD.replaceAll("W",4+"y")
+let XD = WD.replaceAll("X",5+"y")
+let YD = XD.replaceAll("Y",6+"y")
+let ZD = YD.replaceAll("Z",7+"y")
+let ReplacedItems2 = ZD
 
+let msmD3 = document.getElementById("msmDescripto");
+
+msmD3.innerHTML = ReplacedItems2
 }
