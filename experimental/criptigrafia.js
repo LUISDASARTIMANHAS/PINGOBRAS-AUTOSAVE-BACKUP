7712b245dd38a95332cbaf1ce9ca40b25b583614
Glitch (pingobras) <none>
2022-11-21 19:34:48 +0000

    👴🐆 Checkpoint ./experimental/criptigrafia.js:26026384/2515 ./cripto.html:26026384/2

--- experimental/criptigrafia.js
+++ experimental/criptigrafia.js
@@ -1,4 +1,5 @@
 const loopCripto = setInterval(refreshCripto,1000)
+const loopDescripto = setInterval(refreshDescripto,1000)
 
 function refreshCripto(){
 const InpASerCripto = document.getElementById("msmASerCripto").value;
@@ -34,11 +35,13 @@ const ReplacedItems = ZR
 const msmC = document.getElementById("msmCripto");
 
 msmC.innerHTML = ReplacedItems
-//Fim do criptografo
-  
-  let InpASerDescripto = document.getElementById("msmASerDescripto").value;
+}//Fim do criptografo
+
+
+function refreshDescripto(){
+let InpASerDescripto = document.getElementById("msmASerDescripto").value;
 let Criptografar2 = InpASerDescripto.toUpperCase()
-let AD = Criptografar.replaceAll(0,"A")
+let AD = Criptografar2.replaceAll(0,"A")
 let BD = AD.replaceAll(1,"B")
 let CD = BD.replaceAll(2,"C")
 let DD = CD.replaceAll(3,"D")
@@ -46,24 +49,24 @@ let ED = DD.replaceAll(4,"E")
 let FD = ED.replaceAll(5,"F")
 let GD = FD.replaceAll(6,"G")
 let HD = GD.replaceAll(7,"H")
-let ID = HD.replaceAll("I",8)
-let JD = ID.replaceAll("J",9)
-let KD = JD.replaceAll("K",1+"x")
-let LD = KD.replaceAll("L",2+"x")
-let MD = LD.replaceAll("M",3+"x")
-let ND = MD.replaceAll("N",4+"x")
-let OD = ND.replaceAll("O",5+"x")
-let PD = OD.replaceAll("P",6+"x")
-let QD = PD.replaceAll("Q",7+"x")
-let RD = QD.replaceAll("R",8+"x")
-let SD = RD.replaceAll("S",9+"x")
-let TD = SD.replaceAll("T",1+"y")
-let UD = TD.replaceAll("U",2+"y")
-let VD = UD.replaceAll("V",3+"y")
-let WD = VD.replaceAll("W",4+"y")
-let XD = WD.replaceAll("X",5+"y")
-let YD = XD.replaceAll("Y",6+"y")
-let ZD = YD.replaceAll("Z",7+"y")
+let ID = HD.replaceAll(8,"I")
+let JD = ID.replaceAll(9,"J")
+let KD = JD.replaceAll(1+"x","K")
+let LD = KD.replaceAll(2+"x","L")
+let MD = LD.replaceAll(3+"x","M")
+let ND = MD.replaceAll(4+"x","N")
+let OD = ND.replaceAll(5+"x","O")
+let PD = OD.replaceAll(6+"x","P")
+let QD = PD.replaceAll(7+"x","Q")
+let RD = QD.replaceAll(8+"x","R")
+let SD = RD.replaceAll(9+"x","S")
+let TD = SD.replaceAll(1+"y","T")
+let UD = TD.replaceAll(2+"y","U")
+let VD = UD.replaceAll(3+"y","V")
+let WD = VD.replaceAll(4+"y","W")
+let XD = WD.replaceAll(5+"y","X")
+let YD = XD.replaceAll(6+"y","Y")
+let ZD = YD.replaceAll(7+"y","Z")
 let ReplacedItems2 = ZD
 
 let msmD3 = document.getElementById("msmDescripto");
