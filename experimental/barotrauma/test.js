d8683e964893ebd92f148e3b57aa279b9cfd4c83
Glitch (pingobras) <none>
2022-12-02 11:09:15 +0000

    👧☔️ Checkpoint ./experimental/barotrauma/test.js:26026384/459

--- experimental/barotrauma/test.js
+++ experimental/barotrauma/test.js
@@ -2,8 +2,21 @@ var canvas = document.getElementById("GameBoard");
 var context = canvas.getContext("2d");
 
 //desenhar game
+let vertical = 10
+let horizontal = 10
+let TamLargura = 30 
+let TamAltura = 25
 context.fillStyle = "red"
-context.fillRect(50,50,100,100)
+context.fillRect(horizontal,vertical,TamLargura,TamAltura)
 
 // salvando configs.
 context.save()
+
+vertical = 10
+horizontal = 50
+TamLargura = 30
+TamAltura = 25
+context.fillStyle = "blue"
+context.fillRect(horizontal,vertical,TamLargura,TamAltura)
+
+context.rest
