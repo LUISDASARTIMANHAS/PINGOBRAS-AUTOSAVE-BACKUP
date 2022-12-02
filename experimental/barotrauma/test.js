9e29b7155b36c3d94708edec006e0514ae1db4f1
Glitch (pingobras) <none>
2022-12-02 11:56:36 +0000

    ☄️🏣 Checkpoint ./experimental/barotrauma/test.js:26026384/848

--- experimental/barotrauma/test.js
+++ experimental/barotrauma/test.js
@@ -1,13 +1,15 @@
 var canvas = document.getElementById("GameBoard");
 var context = canvas.getContext("2d");
+let meioARC = Math.PI/180
 
+/*
 //desenhar game
 let vertical = 10
 let horizontal = 10
 let TamLargura = 30 
 let TamAltura = 25
 context.fillStyle = "red"
-//context.fillRect(horizontal,vertical,TamLargura,TamAltura)
+context.fillRect(horizontal,vertical,TamLargura,TamAltura)
 
 // salvando configs.
 context.save()
@@ -18,15 +20,16 @@ horizontal = 50
 TamLargura = 30
 TamAltura = 25
 context.fillStyle = "blue"
-//context.fillRect(horizontal,vertical,TamLargura,TamAltura)
+context.fillRect(horizontal,vertical,TamLargura,TamAltura)
 
 //restaura as configs salvas por save
 horizontal = 100
 context.restore()
-//context.fillRect(horizontal,vertical,TamLargura,TamAltura)
+context.fillRect(horizontal,vertical,TamLargura,TamAltura)*/
 
 //iniciando um novo caminho
-context.beginPath();
+/*
+context.beginPath()
 context.moveTo(75,250)// Ponto inicial
 context.lineTo(150,50)//Percorrer trajeto com uma linha
 context.lineTo(225,250)//Percorrer trajeto com uma linha
@@ -36,7 +39,21 @@ context.lineTo(75,250)//Percorrer trajeto com uma linha
 
 context.lineWidth = 2
 context.strokeStyle  = "red"
-//context.stroke()
+context.stroke()
 
 //circunferencia e arcos
-context.arc(50,50,40,90*Math.PI180,Math.PI / 180 * 270, )
+context.beginPath()
+context.arc(50,50,40,90*meioARC, 270*meioARC,false)
+context.fill()
+context.stroke()
+
+context.beginPath()
+context.arc(50,50,40,90*meioARC, 270*meioARC,true)
+context.fill()
+context.stroke()
+*/
+
+context.beginPath()
+context.arc(250,50,40, 0,2*meioARC)
+context.fill()
+context.stroke()
