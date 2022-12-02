a1622de4c8dedc24db95e1b52c77bea1c12ebec5
Glitch (pingobras) <none>
2022-12-02 10:58:10 +0000

    🤑🏹 Checkpoint ./experimental/barotrauma/style.css:26026384/88 ./experimental/barotrauma/index.html:26026384/40 ./experimental/barotrauma/test.js:26026384/123

--- experimental/barotrauma/test.js
+++ experimental/barotrauma/test.js
@@ -5,6 +5,5 @@ var context = canvas.getContext("2d");
 context.fillStyle = "red"
 context.fillRect(50,50,100,100)
 
-context.lineWidth = 10
-context.strokeStyle = "green"
-context.strokeRect(50,50,100,100)
+// salvando configs.
+context.save()
