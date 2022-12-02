66df606f7edc3e94f3e4eb54e25471729ff54c6f
Glitch (pingobras) <none>
2022-12-02 11:44:31 +0000

    🎻🎹 Checkpoint ./experimental/barotrauma/test.js:26026384/251

--- experimental/barotrauma/test.js
+++ experimental/barotrauma/test.js
@@ -7,7 +7,7 @@ let horizontal = 10
 let TamLargura = 30 
 let TamAltura = 25
 context.fillStyle = "red"
-context.fillRect(horizontal,vertical,TamLargura,TamAltura)
+//context.fillRect(horizontal,vertical,TamLargura,TamAltura)
 
 // salvando configs.
 context.save()
@@ -18,12 +18,12 @@ horizontal = 50
 TamLargura = 30
 TamAltura = 25
 context.fillStyle = "blue"
-context.fillRect(horizontal,vertical,TamLargura,TamAltura)
+//context.fillRect(horizontal,vertical,TamLargura,TamAltura)
 
 //restaura as configs salvas por save
 horizontal = 100
 context.restore()
-context.fillRect(horizontal,vertical,TamLargura,TamAltura)
+//context.fillRect(horizontal,vertical,TamLargura,TamAltura)
 
 //iniciando um novo caminho
 context.beginPath();
@@ -32,7 +32,11 @@ context.lineTo(150,50)//Percorrer trajeto com uma linha
 context.lineTo(225,250)//Percorrer trajeto com uma linha
 context.lineTo(50,120)//Percorrer trajeto com uma linha
 context.lineTo(250,120)//Percorrer trajeto com uma linha
-context.lineTo(150,50)//Percorrer trajeto com uma linha
+context.lineTo(75,250)//Percorrer trajeto com uma linha
 
 context.lineWidth = 2
-context.
+context.strokeStyle  = "red"
+//context.stroke()
+
+//circunferencia e arcos
+context.arc(50,50,40,90*Math.PI180,Math.PI / 180 * 270, )
