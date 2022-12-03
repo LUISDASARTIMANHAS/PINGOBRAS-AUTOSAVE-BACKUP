ca614d6ee00073d8c0a08686e89d429c4576285c
Glitch (pingobras) <none>
2022-12-03 17:29:45 +0000

    🚇🎑 Checkpoint ./user/live/live.js:26026384/1456 ./user/live/futebol.html:26026384/1029 ./experimental/embutirVid.html:26026384/219 ./js/links.js:26026384/448

--- js/links.js
+++ js/links.js
@@ -12,7 +12,8 @@ links.innerHTML =
 "<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/head.css />"+
 "<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/RGB.css />"+
 "<link rel=stylesheet rel=preload href=https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css>"+
-"<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/main.css>"
+"<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/main.css>"+
+"<link rel=stylesheet href=https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css integrity=sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6 crossorigin=anonymous>"
 
 
 function refreshlinks() {
@@ -22,6 +23,7 @@ links.innerHTML =
 "<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/head.css />"+
 "<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/RGB.css />"+
 "<link rel=stylesheet rel=preload href=https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css>"+
-"<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/main.css>"
+"<link rel=stylesheet rel=preload href=https://pingobras.glitch.me/css/main.css>"+
+"<link rel=stylesheet href=https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css integrity=sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6 crossorigin=anonymous>"
 
 }//Fim do refresh Links
