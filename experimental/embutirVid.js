6fda0a166b7cc234a96b307f2533eaf6f2fd325c
Glitch (pingobras) <none>
2022-12-03 17:52:47 +0000

    🦃🌌 Checkpoint ./experimental/embutirVid.html:26026384/497 ./experimental/embutirVid.js:26026384/1611

--- experimental/embutirVid.js
+++ experimental/embutirVid.js
@@ -1,43 +1,44 @@
-const videoL = document.getElementById("video")
 const INPL = document.getElementById("INPL")
-const iframe = document.getElementById("iframe")
+const VIDEOPLAYERLINK = document.getElementById("VIDEOPLAYERLINK")
+const VIDEOPLAYER = document.getElementById("VIDEOPLAYER")
 const title = document.getElementById("title")
-videoL.addEventListener("keyup", searchVideo);
-    
+INPL.addEventListener("keyup", searchVideo);
+const LINKPADRAO = "https://www.youtube.com/embed"
+const LINKNovoAUTO = LINK.replace("watch?v=", "embed/")+"?autoplay=1"
     
 function searchVideo(){
-  let video = videoL.value
+  let LINK = INPL.value
   const autoplay = document.getElementById("autoplay").checked
   
   
-  if(autoplay == false){
-    const videoNovo = video.replace("watch?v=", "embed/")
+if(autoplay == false){
+const LINKNovo = LINK.replace("watch?v=", "embed/")
     
-    if(video == ""||video == " "){
-    let videoNovo2 = "https://www.youtube.com/embed"
+if(LINK == ""||LINK == " "){
+
+
+ console.error("Video Não Indentificado!")
+ VIDEOPLAYERLINK.placeholder = LINKPADRAO;
+ VIDEOPLAYER.setAttribute('src', LINKPADRAO);
+ title.innerHTML = "PINGOBRAS EMBUTIR VIDEO?NDA"
+}else{
+  VIDEOPLAYERLINK.placeholder = LINKNovo
+  VIDEOPLAYER.setAttribute('src', LINKNovo)
+  title.innerHTML = "PINGOBRAS?" + LINKNovo}
     
-    console.error("Video Não Indentificado!")
-    INPL.placeholder = videoNovo2;
-    iframe.setAttribute('src', videoNovo2);
-    title.innerHTML = "PINGOBRAS EMBUTIR VIDEO?NDA"
   }else{
-  INPL.placeholder = videoNovo
-  iframe.setAttribute('src', videoNovo)
-  title.innerHTML = "PINGOBRAS?" + videoNovo}
-    
-  }else{
-  const videoNovo = video.replace("watch?v=", "embed/")+"?autoplay=1"
+
   
   if(video == ""||video == " "){
     let videoNovo2 = "https://www.youtube.com/embed"
     
     console.error("Video Não Indentificado!")
-    INPL.placeholder = videoNovo2;
-    iframe.setAttribute('src', videoNovo2);
+    .placeholder = videoNovo2;
+    VIDEOPLAYER.setAttribute('src', videoNovo2);
     title.innerHTML = "PINGOBRAS EMBUTIR VIDEO?NDA"
   }else{
-  INPL.placeholder = videoNovo
-  iframe.setAttribute('src', videoNovo)
+  VIDEOPLAYERLINK.placeholder = videoNovo
+  VIDEOPLAYER.setAttribute('src', videoNovo)
   title.innerHTML = "PINGOBRAS?" + videoNovo}
     
 }}
