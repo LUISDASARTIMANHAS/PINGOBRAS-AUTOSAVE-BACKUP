b3915e3aa6a9f3bd01c97099580b343a3037c6ff
Glitch (pingobras) <none>
2022-12-04 15:25:44 +0000

    🐧🐚 Checkpoint ./user/live/live.js:26026384/1508

--- user/live/live.js
+++ user/live/live.js
@@ -27,7 +27,7 @@ function FUTLive01(){
 const FUTLink01 = document.getElementById("FUTLink#01")
 const FUTLive01 = document.getElementById("FUTLive#01")
 const dataLink = "&ussport=false&datatracking=false&gacode=UA-43334570-1&bodyclasses=performV2PlayerContainer&csspath=https://assets.cdnppb.net/static/videoplayer/v2.59.0/resources/bf/performV2&Vis3D=true&widgetvis=true&imageconfig=https://assets.cdnppb.net/static/videoplayer/v2.59.0/resources/bf/performV2/imageconfig.js&lang=pt&streamonly=true&mode=mini&height=520&defaultview=vis&used=true"
-let tokenLink = "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcklkIjoiNyIsInVzZXJJZCI6IjciLCJzcG9ydGlkIjoiMSIsImdzbWlkIjoiMzk5Mzg0NSIsIndiaWQiOiIwIiwicmJpZCI6IjE3NDMyMzMiLCJ1dWlkIjoiYWFudDZwdnpwOXVobm02eHZ1eWNvaGRlYyIsImlhdCI6MTY3MDA4NzgwNiwiZXhwIjoxNjcwMTAyMjA2LCJpc3MiOiJjc2IifQ.tt1i6BmWIFy2EcJMISrSZn8uGWeWo9_yfnaNzpQ-qRg"
+let tokenLink = "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcklkIjoiNyIsInVzZXJJZCI6IjciLCJzcG9ydGlkIjoiMSIsImdzbWlkIjoiMzI5ODk3NyIsIndiaWQiOiIwIiwicmJpZCI6IjE3MjE5NjMiLCJ1dWlkIjoiMjFjNHBtZDYxZ2txaHFvOHV4d3JrN25mdSIsImlhdCI6MTY3MDE2NjU2MCwiZXhwIjoxNjcwMTgwOTYwLCJpc3MiOiJjc2IifQ.UwvmRjFgh2OOd9Ruy_st83st6IlVhyOgYgTjCrrx0Fk"
 const linkDTrasmissao = "//wab-visualisation.performgroup.com/csb/index.html?" + tokenLink + dataLink
     
   FUTLink01.placeholder = linkDTrasmissao
