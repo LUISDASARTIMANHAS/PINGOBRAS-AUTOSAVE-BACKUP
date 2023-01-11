a4206eec15ddbedf7ac2bfb47a5d2ac4d3a6ae18
Glitch (pingobras) <none>
2023-01-11 19:10:17 +0000

    Rewound to commit 415c6f0a68295236293c30a237fa1ef6f4e71168 + 0 edits

--- js/worker.js
+++ js/worker.js
@@ -1,64 +1,32 @@
-const RegistroLink1 = "js/worker.js"
-const RegistroLink2 = "js/functions.js"
-const RegistroLink3 = "js/network.js"
-const RegistroLink4 = "js/copyright.js"
-const RegistroLink5 = "https://pingobras.glitch.me/js/base.js"
-
-
 if ("serviceWorker" in navigator) {
-	console.log("Registrando Um Js...");
+	console.log("Registrando worker da network...");
 	navigator.serviceWorker
-	.register(RegistroLink1)
-		
-    .then(function (reg) {
-			if (navigator.serviceWorker.controller === null) {
-				navigator.serviceWorker.ready.then(() => {reg.active.postMessage("claimMe");});}
-	      console.log("Script Js Registrado Para Todo O Domínio!!");
-	  	  })
-		
-    .catch(function (err) {console.error("Erro Ao Registrar Um js: ", err);});
-}//Fim Do Registrador 
-
-
-if ("serviceWorker" in navigator) {
-	console.log("Registrando Um Js...");
-	navigator.serviceWorker
-  .register(RegistroLink2)
-  
+		.register("js/network.js")
 		.then(function (reg) {
 			if (navigator.serviceWorker.controller === null) {
-				navigator.serviceWorker.ready.then(() => {reg.active.postMessage("claimMe");});}
-			console.log("Script Js Registrado Para Todo O Domínio!");
-  })
-		.catch(function (err) {console.error("Erro Ao Registrar Um Js: ", err);});
-}//Fim Do Registrador
-
-
-if ("serviceWorker" in navigator) {
-	console.log("Registrando Um Js...");
-	navigator.serviceWorker
-  .register(RegistroLink3)
-  
-		.then(function (reg) {
-			if (navigator.serviceWorker.controller === null) {
-				navigator.serviceWorker.ready.then(() => {reg.active.postMessage("claimMe");});}
-			console.log("Script Js Registrado Para Todo O Domínio!");
+				navigator.serviceWorker.ready.then(() => {
+					reg.active.postMessage("claimMe");
+				});
+			}
+			console.log("Worker network registrado!");
 		})
-  
-		.catch(function (err) {console.error("Erro Ao Registrar Um js: ", err);});
-}//Fim Do Registrador
-
-
+		.catch(function (err) {
+			console.log("Erro ao registrar worker: ", err);
+		});
+}
 if ("serviceWorker" in navigator) {
-	console.log("Registrando Um Js...");
+	console.log("Registrando worker...");
 	navigator.serviceWorker
-  .register(RegistroLink4)
-  
+    .register("js/worker.js")
 		.then(function (reg) {
 			if (navigator.serviceWorker.controller === null) {
-				navigator.serviceWorker.ready.then(() => {reg.active.postMessage("claimMe");});}
-			console.log("Script Js Registrado Para Todo O Domínio!");
+				navigator.serviceWorker.ready.then(() => {
+					reg.active.postMessage("claimMe");
+				});
+			}
+			console.log("Worker js registrado!");
 		})
-  
-		.catch(function (err) {console.error("Erro Ao Registrar Um js: ", err);});
-}//Fim Do Registrador
+		.catch(function (err) {
+			console.log("Erro ao registrar worker: ", err);
+		});
+}
