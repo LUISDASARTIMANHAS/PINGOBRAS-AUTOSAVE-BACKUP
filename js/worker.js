72fa932f4a1c8810bc639e284ef9e5e624e12789
Glitch (pingobras) <none>
2023-01-11 18:24:46 +0000

    🎠📹 Checkpoint ./index.html:26026384/41169 ./js/worker.js:26026384/123

--- js/worker.js
+++ js/worker.js
@@ -11,40 +11,33 @@ if ("serviceWorker" in navigator) {
 			console.log("Network js registrado para todo o dominio!!");
 		})
 		.catch(function (err) {
-			console.log("Erro ao registrar Network js: ", err);
+			console.error("Erro ao registrar Network js: ", err);
 		});
 }
 
 if ("serviceWorker" in navigator) {
 	console.log("Registrando worker...");
 	navigator.serviceWorker
-    .register("js/worker.js")
+  .register("js/worker.js")
+  
 		.then(function (reg) {
 			if (navigator.serviceWorker.controller === null) {
-				navigator.serviceWorker.ready.then(() => {
-					reg.active.postMessage("claimMe");
-				});
-			}
+				navigator.serviceWorker.ready.then(() => {reg.active.postMessage("claimMe");});}
 			console.log("Worker js registrado para todo o dominio!");
-		})
-		.catch(function (err) {
-			console.log("Erro ao registrar worker: ", err);
-		});
+  })
+		.catch(function (err) {console.error("Erro ao registrar worker: ", err);});
 }
 
 if ("serviceWorker" in navigator) {
 	console.log("Registrando funções js...");
 	navigator.serviceWorker
     .register("js/functions.js")
+  
 		.then(function (reg) {
 			if (navigator.serviceWorker.controller === null) {
-				navigator.serviceWorker.ready.then(() => {
-					reg.active.postMessage("claimMe");
-				});
-			}
+				navigator.serviceWorker.ready.then(() => {reg.active.postMessage("claimMe");});}
 			console.log("funções js registrado para todo o dominio!");
 		})
-		.catch(function (err) {
-			console.log("Erro ao registrar funções js: ", err);
-		});
+  
+		.catch(function (err) {console.error("Erro ao registrar funções js: ", err);});
 }
