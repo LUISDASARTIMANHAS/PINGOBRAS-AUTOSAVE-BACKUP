26570e103bc320425a94d8621840bea327afe053
Glitch (pingobras) <none>
2023-01-13 16:56:34 +0000

    🌏💧 Checkpoint ./Factorio_Server/status.js:26026384/68

--- Factorio_Server/status.js
+++ Factorio_Server/status.js
@@ -3,15 +3,15 @@ const StatusLabel = document.getElementById("status-Server")
 const saveHostLabel = document.getElementById("save-Host")
 const Porta = ":21029"
 
-let IP = "18.230.184.245"
-let Mundo = "FileTorradao Multi"
-let sizeMundo = 5.0
+let IP = ""
+let Mundo = ""
+let sizeMundo = 0
 
 if(IP == ""||!IP){
   StatusLabel.innerHTML = "Parado"
   StatusLabel.style.color = "red"
   IPServer.innerHTML = "SEM IP DO SERVIDOR"
-  saveHostLabel.innerHTML = "HUB"
+  saveHostLabel.innerHTML = "HUB" + " (1.1.70) " + sizeMundo+"MB"
 }else{
   StatusLabel.innerHTML = "Online"
   StatusLabel.style.color = "green"
