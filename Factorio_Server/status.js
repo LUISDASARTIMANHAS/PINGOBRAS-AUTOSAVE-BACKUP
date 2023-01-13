a51b688f9c0d59fa83a2cba9a20b2686b8d8ef45
Glitch (pingobras) <none>
2023-01-13 02:12:12 +0000

    ☂️🚕 Checkpoint ./Factorio_Server/status.js:26026384/355 ./Factorio_Server/index.html:26026384/393 ./Factorio_Server/server.html:26026384/76

--- Factorio_Server/status.js
+++ Factorio_Server/status.js
@@ -4,5 +4,11 @@ const StatusLabel = document.getElementById("status-Server")
 let IP = "15.228.167.22:21029"
 
 if(IP == ""||!IP){
-  
+  StatusLabel.innerHTML = "Parado"
+  StatusLabel.style.color = "red"
+  IPServer.innerHTML = "SEM IP DO SRVIDOR"
+}else{
+  StatusLabel.innerHTML = "Online"
+  StatusLabel.style.color = "green"
+  IPServer.innerHTML = IP
 }
